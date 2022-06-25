cea66a1c80364ddea919266d004ac88c389dfe0a
Nick Mackenzie <nmackenzie@NBIPANESAR.CO.KE>
2022-06-25 15:46:30 +0300

    Initial project setup

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+//DB
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','');
+//APP ROOT
+define('APPROOT',dirname(dirname(__FILE__)));
+//url root
+define('URLROOT','http://localhost/pcea_tee');
+//SITE NAME
+define('SITENAME','PCEA TEE');
