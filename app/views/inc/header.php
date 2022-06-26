351529cec4cf1839b08a6fe255cdf4885296210c
Nick Mackenzie <nmackenzie@NBIPANESAR.CO.KE>
2022-06-26 16:59:53 +0300

    Add app styles and js files

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo SITENAME; ?></title>
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+    <title><?php echo $data['PageName']; ?> | PCEA TEE</title>
+    <link rel="shortcut icon" href="./img/logos/favicon.ico" type="image/x-icon">
+    <!-- Third Page stylesheets -->
+    <link rel="stylesheet" href="./css/app.min.css">
+
+    <!-- Our styles-->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     
