8c3acad5ff2377c4a657339352b7fa0aa5693cba
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-27 09:09:21 +0300

    Reusable footer for auth pages

---
+++ app/views/inc/layout/auth/footer.php
@@ -0,0 +1,19 @@
+<!-- end card -->
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end page -->
+
+        <footer class="footer footer-alt">
+            <?php echo date("Y");?> © Mack Softwares Solution
+        </footer>
+
+        <!-- bundle -->
+        <script src="./dist/js/vendor.min.js"></script>
+        <script src="./dist/js/app.min.js"></script>
+        
+    </body>
+</html>
