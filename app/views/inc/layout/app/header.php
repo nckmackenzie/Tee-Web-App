23b0497053122527272bdfe40f141dd61815cdd0
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-27 11:51:41 +0300

    Folder restructuring for app layout

---
+++ app/views/inc/layout/app/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $data['PageName']; ?> | PCEA TEE</title>
+    <link rel="shortcut icon" href="./img/logos/favicon.ico" type="image/x-icon">
+    <!-- Third Page stylesheets -->
+    <link rel="stylesheet" href="./css/app.min.css">
+
+    <!-- Our styles-->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    
