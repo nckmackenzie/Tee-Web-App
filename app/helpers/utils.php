5cabf64b2fe8978b76cfac9da5f3b5ca2c8d11c4
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-28 11:57:13 +0300

    Addition - inputvalidation function for adding css validation classes

--- app/helpers/utils.php
+++ app/helpers/utils.php
@@ -3,3 +3,12 @@
 function redirect($page){
     header('location: ' . URLROOT . '/' . $page);
 }
+
+//add css classes for data validation
+function inputvalidation($data,$err){
+    if (!empty($err)){
+        return 'is-invalid';
+    }elseif (empty($err) && !empty($data)){ 
+        return 'is-valid';
+    }
+}
