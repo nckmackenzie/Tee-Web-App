1dbbd2932f216f70ca3f9dcb62179fd754dead35
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-28 11:13:35 +0300

    Add DB Name

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','');
+define('DB_NAME','tee');
 //APP ROOT
 define('APPROOT',dirname(dirname(__FILE__)));
 //url root
