4b896ee6d07c3a7f4e5e8abe049566d280df7b93
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-29 11:39:17 +0300

    Toast Component

---
+++ app/views/inc/ui/Toast.php
@@ -0,0 +1,15 @@
+<div class="toast-container position-fixed top-0 start-50 translate-middle-x p-3">
+  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header bg-primary">
+      <img src="./check_24px.png" class="rounded me-2" alt="SsaaSASD">
+      <strong class="me-auto text-white">Bootstrap</strong>
+      <small class="text-white">Now</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      Hello, world! This is a toast message.
+    </div>
+  </div>
+</div>
+    
+        
