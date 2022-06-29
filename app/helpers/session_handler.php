976adc8490e3af73061b7023ec82ef7778f1a176
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-29 11:37:18 +0300

    Session handler function to output alert or toasts

---
+++ app/helpers/session_handler.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+    // Flash message helper
+    function flash($name='',$type='',$message='',$class=''){
+        if(!empty($name)){
+            //No message, create it
+            if(!empty($message) && empty($_SESSION[$name])){
+              if(!empty( $_SESSION[$name])){
+                  unset( $_SESSION[$name]);
+              }
+              if(!empty( $_SESSION[$name.'_class'])){
+                  unset( $_SESSION[$name.'_class']);
+              }
+              $_SESSION[$name] = $message;
+              $_SESSION[$name.'_class'] = $class;
+            }
+            //Message exists, display it
+            elseif(!empty($_SESSION[$name]) && empty($message) && $type === 'alert'){
+              $class = !empty($_SESSION[$name.'_class']) ? $_SESSION[$name.'_class'] : 'success';
+              echo '<div class="'.$class.'" id="msg-flash" role="alert">'.$_SESSION[$name].'
+                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>';
+              unset($_SESSION[$name]);
+              unset($_SESSION[$name.'_class']);
+            }elseif(!empty($_SESSION[$name]) && empty($message) && $type === 'toast'){
+              $class = !empty($_SESSION[$name.'_class']) ? $_SESSION[$name.'_class'] : 'success';
+              echo '<div class="toast-container position-fixed top-0 start-50 translate-middle-x p-3">
+                      <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                        <div class="'.$class.'">
+                          <img src="'.URLROOT.'./img/other/info_24px.png" class="rounded me-2" alt="Action Icon">
+                          <strong class="me-auto text-white">'.SITENAME.'</strong>
+                          <small class="text-white">Now</small>
+                          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                        <div class="toast-body">
+                        '.$_SESSION[$name].'
+                        </div>
+                      </div>
+                    </div>
+                    <script>
+                      const toastLiveExample = document.getElementById("liveToast")
+                      const toast = new bootstrap.Toast(toastLiveExample)
+                      toast.show()
+                    </script>';
+              unset($_SESSION[$name]);
+              unset($_SESSION[$name.'_class']);
+            }
+          }
+    }
