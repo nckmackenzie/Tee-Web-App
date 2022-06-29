974cd9903ad66a77d16b8e7edad941dda93e786b
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-29 08:23:56 +0300

    Addition - require session handler file

--- app/boostrap.php
+++ app/boostrap.php
@@ -3,6 +3,7 @@
 require_once 'config/config.php';
 //load functions
 require_once 'helpers/utils.php';
+require_once 'helpers/session_handler.php';
 // require_once 'libraries/Core.php';
 // require_once 'libraries/Controller.php';
 // require_once 'libraries/Database.php';
