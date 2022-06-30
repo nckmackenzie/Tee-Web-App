845cc70f78f9dd676e1cc0bd6af4e5a266815f70
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-30 10:44:24 +0300

    Addition - CheckUserAvailalbity method to check if user exists and is active

--- app/models/Auths.php
+++ app/models/Auths.php
@@ -3,15 +3,29 @@
 class Auths 
 {
     private $db;
+
     public function __construct()
     {
         $this->db = new Database;
     }
+
     public function LoadCenters()
     {
         $this->db->query("SELECT ID,
                                  UCASE(CenterName) As CenterName
                           FROM   centers WHERE Deleted = 0 ORDER BY CenterName");
         return $this->db->resultset();
+    }
+
+    public function CheckUserAvailability($userid,$center)
+    {
+        $arr = array();
+        array_push($arr,$userid);
+        array_push($arr,(int)$center);
+        if((int)getdbvalue($this->db->dbh,"SELECT checkuseravailability(?,?)",$arr) === 0){
+           return false; 
+        }else{
+            return true;
+        }
     }
 }
