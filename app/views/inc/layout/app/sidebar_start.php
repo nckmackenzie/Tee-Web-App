4bc6f4e151cb9a6d2cb19bdfd22f1a2353d61c53
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-30 14:00:03 +0300

    Sidebar start markup

---
+++ app/views/inc/layout/app/sidebar_start.php
@@ -0,0 +1,3 @@
+<body class="loading" data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
+        <!-- Begin page -->
+        <div class="wrapper">
