2ab038d37e7b08d21dd186fca470047c37fb0823
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-30 11:42:55 +0300

    App header file

--- app/views/inc/layout/app/header.php
+++ app/views/inc/layout/app/header.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $data['PageName']; ?> | PCEA TEE</title>
-    <link rel="shortcut icon" href="./img/logos/favicon.ico" type="image/x-icon">
-    <!-- Third Page stylesheets -->
-    <link rel="stylesheet" href="./css/app.min.css">
+    <head>
+        <meta charset="utf-8" />
+        <title><?php echo $data['title'];?> | P.C.E.A T.E.E</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="<?php echo URLROOT;?>/img/logos/favicon.ico">
+        <!-- Datatable styles -->
+        <?php if(isset($data['has_datatable']) && $data['has_datatable'] === true) : ?>
+            <link href="<?php echo URLROOT;?>/dist/css/vendor/dataTables.bootstrap5.css" rel="stylesheet" type="text/css" />
+            <link href="<?php echo URLROOT;?>/dist/css/vendor/responsive.bootstrap5.css" rel="stylesheet" type="text/css" />
+            <link href="<?php echo URLROOT;?>/dist/css/vendor/buttons.bootstrap5.css" rel="stylesheet" type="text/css" />
+            <link href="<?php echo URLROOT;?>/dist/css/vendor/select.bootstrap5.css" rel="stylesheet" type="text/css" />
+        <?php endif; ?>    
+        <!-- App css -->
+        <link href="<?php echo URLROOT;?>/dist/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo URLROOT;?>/dist/css/app.min.css" rel="stylesheet" type="text/css" id="light-style" />
+        <link href="<?php echo URLROOT;?>/dist/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style" />
+    </head>
 
-    <!-- Our styles-->
-    <link rel="stylesheet" href="./css/style.css">
-</head>
-<body>
     
