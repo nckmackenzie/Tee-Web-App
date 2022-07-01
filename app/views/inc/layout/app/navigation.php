3a7e515f263ae51176433a5674bf3b25ee42ea2a
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-07-01 09:47:48 +0300

    Initial Navigation setup

--- app/views/inc/layout/app/navigation.php
+++ app/views/inc/layout/app/navigation.php
@@ -2,9 +2,9 @@
 <div class="leftside-menu">
     <!-- LOGO -->
     <a href="<?php echo URLROOT;?>/home" class="logo text-center logo-light">
-        <span class="logo-lg d-flex">
+        <span class="logo-lg">
             <img src="<?php echo URLROOT;?>/img/logos/logo_24_white.png" alt="Logo" height="16">
-            <small>P.C.E.A T.E.E</small>
+            <small class="fs-4 text-white">P.C.E.A T.E.E</small>
         </span>
         <span class="logo-sm">
             <img src="<?php echo URLROOT;?>/img/logos/logo_24_white.png" alt="Logo" height="16">
@@ -23,12 +23,12 @@
                 </a>
             </li>
             <li class="side-nav-item">
-                <a data-bs-toggle="collapse" href="#sidebarDashboards" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link">
+                <a data-bs-toggle="collapse" href="#sidebarAdmin" aria-expanded="false" aria-controls="sidebarAdmin" class="side-nav-link">
                     <i class="uil-shield-check"></i>
-                    <span class="badge bg-success float-end">4</span>
                     <span> Admin </span>
+                    <span class="menu-arrow"></span>
                 </a>
-                <div class="collapse" id="sidebarDashboards">
+                <div class="collapse" id="sidebarAdmin">
                     <ul class="side-nav-second-level">
                         <li>
                             <a href="<?php echo URLROOT;?>/users">Users</a>
@@ -36,12 +36,90 @@
                     </ul>
                 </div>
             </li>
-
             <li class="side-nav-item">
-                <a href="<?php echo URLROOT;?>/auth/change_password" class="side-nav-link">
-                    <i class="uil-lock-alt"></i>
-                    <span> Change Password </span>
+                <a data-bs-toggle="collapse" href="#sidebarMaster" aria-expanded="false" aria-controls="sidebarMaster" class="side-nav-link">
+                    <i class="uil-plus-square"></i>
+                    <span> Master Entries </span>
+                    <span class="menu-arrow"></span>
                 </a>
+                <div class="collapse" id="sidebarMaster">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="<?php echo URLROOT;?>/items">Items</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarStocks" aria-expanded="false" aria-controls="sidebarStocks" class="side-nav-link">
+                    <i class="uil-exchange"></i>
+                    <span> Stocks Management </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarStocks">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="<?php echo URLROOT;?>/stocks/receipts">Receipts</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo URLROOT;?>/stocks/transfers">Transfers</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo URLROOT;?>/stocks/sales">Sales</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarStudents" aria-expanded="false" aria-controls="sidebarStudents" class="side-nav-link">
+                    <i class="uil-graduation-hat"></i>
+                    <span> Students </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarStudents">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="<?php echo URLROOT;?>/students/">Students</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarFinance" aria-expanded="false" aria-controls="sidebarFinance" class="side-nav-link">
+                    <i class="uil-moneybag-alt"></i>
+                    <span> Finance </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarFinance">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="<?php echo URLROOT;?>/finances/fees">Fees</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo URLROOT;?>/finances/expenses">Expenses</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo URLROOT;?>/finances/invoices">Invoices</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarReports" aria-expanded="false" aria-controls="sidebarReports" class="side-nav-link">
+                    <i class="uil-receipt-alt"></i>
+                    <span> Reports </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarReports">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="<?php echo URLROOT;?>/reports/fees">Fees Payments</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo URLROOT;?>/reports/feebalances">Fee Balances</a>
+                        </li>
+                    </ul>
+                </div>
             </li>
         </ul><!-- End Sidebar -->
         <div class="clearfix"></div>
