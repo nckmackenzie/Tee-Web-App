649a8a0539a3a3dc6754a1109774aa71c6f8b335
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-07-04 11:09:15 +0300

    Addition - GetUsers function

--- app/models/User.php
+++ app/models/User.php
@@ -7,6 +7,13 @@ class User
         $this->db = new Database;
     }
 
+    public function GetUsers()
+    {
+        $this->db->query("CALL sp_getusers(:cid)");
+        $this->db->bind(':cid',(int)$_SESSION['centerid']);
+        return $this->db->resultset();
+    }
+
     public function ChangeProfile($name)
     {
         $this->db->query("UPDATE users SET UserName= :uname WHERE ID= :id");
