fe4975eea5e2731217c559b85b05b750e575a38a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-04 19:41:22 +0300

    Addition - constructor & index functions

---
+++ app/controllers/Centers.php
@@ -0,0 +1,24 @@
+<?php
+class Centers extends Controller 
+{
+    public function __construct()
+    {
+        if(!isset($_SESSION['userid'])){
+            redirect('auth');
+            exit();
+        }elseif(isset($_SESSION['userid']) && $_SESSION['usertypeid'] > 2 || (int)$_SESSION['ishead'] !=1 ){
+            redirect('auth/forbidden');
+            exit();
+        }else{
+            $this->centermodel = $this->model('Center');
+        }
+    }
+    public function index()
+    {
+        $data = [
+            'title' => 'Centers',
+            'centers' => $this->centermodel->GetCenters()
+        ];
+        $this->view('centers/index', $data);
+    }
+}
