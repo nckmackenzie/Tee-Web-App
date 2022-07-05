d5f3ccb98dea0efffc735155d9599dbd29eeb7bd
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-05 19:12:14 +0300

    Table column changes and flash for toast name renaming

--- app/views/centers/index.php
+++ app/views/centers/index.php
@@ -19,7 +19,7 @@
                 <div class="card-body">
                     <div class="row mb-2">
                         <div class="col-sm-4">
-                            <a href="<?php echo URLROOT;?>/centers/add" class="btn btn-success mb-2"><i class="mdi mdi-plus-circle me-2"></i> Add User</a>
+                            <a href="<?php echo URLROOT;?>/centers/add" class="btn btn-success mb-2"><i class="mdi mdi-plus-circle me-2"></i> Add Center</a>
                         </div>
                     </div>
                     <div class="table-responsive">
@@ -30,18 +30,17 @@
                                     <th>Center Name</th>
                                     <th>Contact</th>
                                     <th>Email</th>
-                                    <th>Address</th>
                                     <th>Status</th>
                                     <th>Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php foreach($centers as $center): ?>
+                                <?php foreach($data['centers'] as $center): ?>
                                     <tr>
                                         <td><?php echo $center->ID;?></td>
+                                        <td><?php echo $center->CenterName;?></td>
                                         <td><?php echo $center->Contact;?></td>
                                         <td><?php echo $center->Email;?></td>
-                                        <td><?php echo $center->Address;?></td>
                                         <td><span class="badge <?php echo badgeclasses($center->Status);?>"><?php echo $center->Status;?></span></td>
                                         <td>
                                             <a href="<?php echo URLROOT;?>/centers/edit/<?php echo $center->ID;?>" class="action-icon btn text-success"> <i class="mdi mdi-square-edit-outline"></i></a>
@@ -58,6 +57,6 @@
     </div><!--End row-->                    
 </div> <!-- container -->
 <?php require APPROOT .'/views/inc/layout/app/footer.php'; ?> 
-<?php flash('center_msg','flash'); ?> 
-<script src="<?php echo URLROOT;?>/dist/js/pages/centers.js"></script>                  
+<?php flash('center_toast_msg','flash'); ?> 
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/centers.js"></script>                  
 <?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
