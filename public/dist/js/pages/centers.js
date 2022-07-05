2147a1c4788a366d2fa6d513be590ed2d8edfa40
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-05 22:08:58 +0300

    Datatable and ID set to input

--- public/dist/js/pages/centers.js
+++ public/dist/js/pages/centers.js
@@ -1,3 +1,26 @@
-import { datatable } from './datatable.js';
+$(document).ready(function () {
+  'use strict';
+  $('#centers-datatable').DataTable({
+    language: {
+      paginate: {
+        previous: "<i class='mdi mdi-chevron-left'>",
+        next: "<i class='mdi mdi-chevron-right'>",
+      },
+      info: 'Showing Centers _START_ to _END_ of _TOTAL_',
+      lengthMenu:
+        'Display <select class=\'form-select form-select-sm ms-1 me-1\'><option value="5">5</option><option value="10">10</option><option value="20">20</option><option value="-1">All</option></select> products',
+    },
+    pageLength: 10,
+    drawCallback: function () {
+      $('.dataTables_paginate > .pagination').addClass('pagination-rounded'),
+        $('#centers-datatable_length label').addClass('form-label');
+    },
+  });
+});
 
-datatable('centers');
+function rowFunction(el) {
+  const input = document.querySelector('#id');
+  var n = el.parentNode.parentNode.cells[0].textContent;
+  input.value = +n;
+  // ...
+}
