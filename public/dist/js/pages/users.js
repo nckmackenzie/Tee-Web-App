7b045b0975ce39f0a9d1210f801ba252cd5995f1
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-07-05 12:38:54 +0300

    Addition - row function to get value of first cell

--- public/dist/js/pages/users.js
+++ public/dist/js/pages/users.js
@@ -17,3 +17,10 @@ $(document).ready(function () {
     },
   });
 });
+
+function rowFunction(el) {
+  const input = document.querySelector('#id');
+  var n = el.parentNode.parentNode.cells[0].textContent;
+  input.value = +n;
+  // ...
+}
