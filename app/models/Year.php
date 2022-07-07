a1813f43f07abd5e5c106e7dfcb870ae9236dbc9
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-07-07 10:59:18 +0300

    Addition - CreateUpdate function

--- app/models/Year.php
+++ app/models/Year.php
@@ -15,4 +15,46 @@ class Year
         $this->db->query('CALL sp_getyears()');
         return $this->db->resultset();
     }
+
+    //check if year exists
+    public function CheckAvailability($field,$id,$param)
+    {
+        if($field === 'name'){
+            $this->db->query('SELECT COUNT(ID) 
+                              FROM   years 
+                              WHERE  (ID <> :id) AND (Deleted = 0) AND (YearName = :field)');
+        }else{
+            $this->db->query('SELECT COUNT(ID) 
+                              FROM   years 
+                              WHERE  (ID <> :id) AND (Deleted = 0) AND (:field BETWEEN StartDate AND EndDate)');
+        }
+        $this->db->bind(":id",$id);
+        $this->db->bind(':field', $field === 'name' ? strtolower(trim($param)) : $param);
+        if((int)$this->db->getvalue() > 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    //save and update
+    public function CreateUpdate($data)
+    {
+        if(!$data['isedit']){
+            $this->db->query('INSERT INTO years (YearName,StartDate,EndDate) VALUES(:yname,:startd,:endd)');
+        }else{
+            $this->db->query('UPDATE years SET YearName=:yname,StartDate=:startd,EndDate=:endd WHERE (ID=:id)');
+        }
+        $this->db->bind(':yname',strtolower(trim($data['name'])));
+        $this->db->bind(':startd',$data['start']);
+        $this->db->bind(':endd',$data['end']);
+        if($data['isedit']){
+            $this->db->bind(':id',$data['id']);
+        }
+        if(!$this->db->execute()) {
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
