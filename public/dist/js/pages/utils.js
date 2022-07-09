40bfcd21a1c545525026cc31410eb776da722ec5
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-07-09 10:13:25 +0300

    reusable btnclick to get and set value of button

--- public/dist/js/pages/utils.js
+++ public/dist/js/pages/utils.js
@@ -1,5 +1,10 @@
-export function rowFunction(el) {
-  const input = document.querySelector('#id');
-  var n = el.parentNode.parentNode.cells[0].textContent;
-  input.value = +n;
+export function btnClick(btnclass, inputid) {
+  const input = document.querySelector('#' + inputid + '');
+  const btns = document.querySelectorAll(btnclass);
+  Array.prototype.forEach.call(btns, function addClickListener(btn) {
+    btn.addEventListener('click', function () {
+      const id = this.dataset.id;
+      input.value = id;
+    });
+  });
 }
