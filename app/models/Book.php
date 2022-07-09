768594484d7cf68377b46edd551d32eac8f334ce
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-07-09 09:04:40 +0300

    Addition - functionality to update in Save method

--- app/models/Book.php
+++ app/models/Book.php
@@ -33,23 +33,43 @@ class Book
             //begin transaction
             $this->db->dbh->beginTransaction();
 
-            $this->db->query('INSERT INTO books (Title,BookCode,Author,Publisher) VALUES(:title, :code, :author, :publisher)');
-            $this->db->bind(':title',$data['name']);
-            $this->db->bind(':code',$data['code']);
-            $this->db->bind(':author',!empty($data['author']) ? $data['author'] : null);
-            $this->db->bind(':publisher',!empty($data['publisher']) ? $data['publisher'] : null);
-            $this->db->execute();
+            if(!$data['isedit']){
+                $this->db->query('INSERT INTO books (Title,BookCode,Author,Publisher) VALUES(:title, :code, :author, :publisher)');
+                $this->db->bind(':title',$data['name']);
+                $this->db->bind(':code',$data['code']);
+                $this->db->bind(':author',!empty($data['author']) ? $data['author'] : null);
+                $this->db->bind(':publisher',!empty($data['publisher']) ? $data['publisher'] : null);
+                $this->db->execute();
 
-            $id = $this->db->dbh->lastInsertId();
+                $id = $this->db->dbh->lastInsertId();
 
-            $this->db->query('INSERT INTO stockmovements (TransactionDate,BookId,Qty,TransactionType,TransactionId) 
-                              VALUES(:tdate,:bid,:qty,:ttype,:tid)');
-            $this->db->bind(':tdate',$data['asat']);
-            $this->db->bind(':bid',$id);
-            $this->db->bind(':qty',$data['openingbal']);
-            $this->db->bind(':ttype',1);
-            $this->db->bind(':tid',$id);
-            $this->db->execute();
+                $this->db->query('INSERT INTO stockmovements (TransactionDate,BookId,Qty,TransactionType,TransactionId) 
+                                  VALUES(:tdate,:bid,:qty,:ttype,:tid)');
+                $this->db->bind(':tdate',$data['asat']);
+                $this->db->bind(':bid',$id);
+                $this->db->bind(':qty',$data['openingbal']);
+                $this->db->bind(':ttype',1);
+                $this->db->bind(':tid',$id);
+                $this->db->execute();
+            }else{
+                if($data['allowedit']){
+                    $this->db->query('UPDATE stockmovements SET TransactionDate = :tdate,Qty = :qty 
+                                      WHERE (BookId = :id) AND (TransactionType = 1)');
+                    $this->db->bind(':tdate',$data['asat']);
+                    $this->db->bind(':qty',$data['openingbal']);
+                    $this->db->bind(':id',$data['id']);
+                    $this->db->execute();
+                }
+                $this->db->query('UPDATE books SET Title = :title, BookCode = :code, Author = :author, Publisher = :publisher, Active = :active 
+                                  WHERE (ID=:id)');
+                $this->db->bind(':title',$data['name']);
+                $this->db->bind(':code',$data['code']);
+                $this->db->bind(':author',!empty($data['author']) ? $data['author'] : null);
+                $this->db->bind(':publisher',!empty($data['publisher']) ? $data['publisher'] : null);
+                $this->db->bind(':active',$data['active']);
+                $this->db->bind(':id',$data['id']);
+                $this->db->execute();
+            }
 
             if(!$this->db->dbh->commit()){
                 return false;
@@ -69,9 +89,7 @@ class Book
     //save or update 
     public function CreateUpdate($data)
     {
-        if(!$data['isedit']){
-            return $this->Save($data);
-        }
+        return $this->Save($data);
     }
 
     //get single book
