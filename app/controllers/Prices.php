1a8c13068005f02ee95e2d69b4a94afcd7ad977d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-11 09:43:42 +0300

    Addition - index method

--- app/controllers/Prices.php
+++ app/controllers/Prices.php
@@ -5,4 +5,15 @@ class Prices extends Controller
         adminonly($_SESSION['userid'],$_SESSION['usertypeid']);
         $this->pricemodel = $this->model('Price');
     }
+
+    public function index()
+    {
+        $prices = $this->pricemodel->GetPrices();
+        $data = [
+            'title' => 'Prices',
+            'has_datatable' => true,
+            'prices' => $prices
+        ];
+        $this->view('prices/index',$data);
+    }
 }
