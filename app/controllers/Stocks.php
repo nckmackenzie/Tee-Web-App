5d023ec33a57f1c087639f18ba55598c666f5b79
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-12 18:09:03 +0300

    constructor setup

---
+++ app/controllers/Stocks.php
@@ -0,0 +1,12 @@
+<?php
+class Stocks extends Controller
+{
+    public function __construct()
+    {
+        if(!is_authenticated($_SESSION['userid'])){
+            redirect('auth');
+            exit();
+        }
+        $this->stockmodel = $this->model('Stock');
+    }
+}
