f325d10c34a2e0126483fce0a23eaead6eb783c2
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-12 19:15:42 +0300

    Addition - GetReceipts method

--- app/models/Stock.php
+++ app/models/Stock.php
@@ -6,4 +6,12 @@ class Stock
     {
         $this->db = new Database;
     }
+
+    //get receipts
+    public function GetReceipts()
+    {
+        $this->db->query('SELECT * FROM vw_receipts WHERE CenterId = :cid');
+        $this->db->bind(':cid',$_SESSION['centerid']);
+        return $this->db->resultset();
+    }
 }
