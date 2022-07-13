0abdab489b3243773a5f3a8e58c7000e1071a217
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-13 21:29:09 +0300

    Adding and removing book to grid dynamically

--- public/dist/js/pages/add-receipts.js
+++ public/dist/js/pages/add-receipts.js
@@ -1,8 +1,12 @@
+import { getSelectedText, HOST_URL } from './utils.js';
+
 const mtnSelect = document.getElementById('mtn');
 const bookSelect = document.getElementById('bookid');
 const receiptDate = document.getElementById('date');
 const valueInput = document.getElementById('value');
 const qtyInput = document.getElementById('qty');
+const btnadd = document.querySelector('.btnadd');
+const table = document.getElementById('receipts-table');
 
 // radio event handler
 document.querySelectorAll("input[name='receipttype']").forEach(input => {
@@ -17,11 +21,10 @@ document.querySelectorAll("input[name='receipttype']").forEach(input => {
 async function getBooksValue() {
   if (!receiptDate || !qtyInput || !bookSelect) return;
 
-  const url = 'http://localhost/pcea_tee';
   const bookValue = bookSelect.value;
   const date = receiptDate.value;
   const res = await fetch(
-    `${url}/stocks/getprice?book=${bookValue}&rdate=${date}`
+    `${HOST_URL}/stocks/getprice?book=${bookValue}&rdate=${date}`
   );
   const data = await res.json();
   let qty = qtyInput.value || 1;
@@ -32,4 +35,36 @@ bookSelect.addEventListener('change', getBooksValue);
 
 qtyInput.addEventListener('change', function (e) {
   getBooksValue();
+});
+
+btnadd.addEventListener('click', function () {
+  if (!bookSelect.value || !qtyInput.value) {
+    alert('Enter all required fields');
+    return;
+  }
+  const rows = table.rows.length;
+  const body = table.getElementsByTagName('tbody')[0];
+  let selectedBook = getSelectedText(bookSelect);
+  let selectedValue = bookSelect.value;
+  let qty = qtyInput.value;
+  let html = `
+      <tr>
+        <td class="d-none">${selectedValue}</td>
+        <td>${selectedBook}</td>
+        <td>${qty}</td>
+        <td><button type="button" class="action-icon btn btn-sm text-danger fs-5 btndel">Remove</button>
+        </td>
+      </tr>
+  `;
+  let newRow = body.insertRow(body.rows.length);
+  newRow.innerHTML = html;
+  bookSelect.value = '';
+  qtyInput.value = '';
+  value.value = '';
+});
+
+table.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('btndel')) return;
+  const btn = e.target;
+  btn.closest('tr').remove();
 });
