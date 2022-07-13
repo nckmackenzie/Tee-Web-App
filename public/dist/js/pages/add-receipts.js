57d25e410cba1d8087b51f83bc2f94d2704770c6
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-13 20:15:47 +0300

    getBooksValue function and event listeners for select and input

--- public/dist/js/pages/add-receipts.js
+++ public/dist/js/pages/add-receipts.js
@@ -1,4 +1,8 @@
 const mtnSelect = document.getElementById('mtn');
+const bookSelect = document.getElementById('bookid');
+const receiptDate = document.getElementById('date');
+const valueInput = document.getElementById('value');
+const qtyInput = document.getElementById('qty');
 
 // radio event handler
 document.querySelectorAll("input[name='receipttype']").forEach(input => {
@@ -8,4 +12,24 @@ document.querySelectorAll("input[name='receipttype']").forEach(input => {
     if (e.target.value === 'internal') mtnSelect.disabled = false;
     mtnSelect.value = '';
   });
+});
+
+async function getBooksValue() {
+  if (!receiptDate || !qtyInput || !bookSelect) return;
+
+  const url = 'http://localhost/pcea_tee';
+  const bookValue = bookSelect.value;
+  const date = receiptDate.value;
+  const res = await fetch(
+    `${url}/stocks/getprice?book=${bookValue}&rdate=${date}`
+  );
+  const data = await res.json();
+  let qty = qtyInput.value || 1;
+  valueInput.value = +qty * +data;
+}
+
+bookSelect.addEventListener('change', getBooksValue);
+
+qtyInput.addEventListener('change', function (e) {
+  getBooksValue();
 });
