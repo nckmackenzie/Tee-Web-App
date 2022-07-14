086bb1b5d2a474528e8e5e904ebac6b7945a1ca8
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-14 22:52:58 +0300

    Saving transfer functionality

--- app/controllers/Stocks.php
+++ app/controllers/Stocks.php
@@ -85,5 +85,102 @@ class Stocks extends Controller
             'transfers' => $transfers,
         ];
         $this->view('stocks/transfers',$data);
+        exit();
+    }
+
+    public function addtransfer()
+    {
+        $books = $this->stockmodel->GetBooks();
+        $centers = $this->stockmodel->GetCenters();
+        $data = [
+            'title' => 'Add Transfer',
+            'centers' => $centers,
+            'books' => $books,
+            'touched' => false,
+            'date' => date('Y-m-d'),
+            'error' => '',
+            'center' => '',
+            'mtn' => '',
+            'table' => [],
+            'date_err' => '',
+            'center_err' => '',
+            'mtn_err' => '',
+        ];
+        $this->view('stocks/addtransfer',$data);
+        exit();
+    }
+
+    public function createupdatetransfer()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $_POST = filter_input_array(INPUT_POST, FILTER_UNSAFE_RAW);
+            $books = $this->stockmodel->GetBooks();
+            $centers = $this->stockmodel->GetCenters();
+            $data = [
+                'title' => 'Add Transfer',
+                'centers' => $centers,
+                'books' => $books,
+                'touched' => true,
+                'error' => '',
+                'date' => !empty($_POST['date']) ? date('Y-m-d',strtotime($_POST['date'])) : '',
+                'center' => !empty($_POST['center']) ? trim($_POST['center']) : '',
+                'mtn' => trim($_POST['mtn']),
+                'table' => [],
+                'booksid' => $_POST['booksid'],
+                'booksname' => $_POST['booksname'],
+                'qtys' => $_POST['qtys'],
+                'date_err' => '',
+                'center_err' => '',
+                'mtn_err' => '',
+            ];
+
+            if(count($data['booksid']) == 0){
+                $data['error'] = 'No items added for transfer';
+            }else{
+                for ($i=0; $i < count($data['booksid']); $i++) { 
+                    array_push($data['table'],[
+                        'pid' => $data['booksid'][$i],
+                        'book' => $data['booksname'][$i],
+                        'qty' => $data['qtys'][$i],
+                    ]);
+                }
+            }
+
+            if(empty($data['date'])){
+                $data['date_err'] = 'Select transfer date';
+            }else{
+                if($data['date'] > date('Y-m-d')){
+                    $data['date_err'] = 'Invalid date selected';
+                }
+            }
+
+            if(empty($data['center'])){
+                $data['center_err'] = 'Select center transfering to';
+            }
+
+            if(empty($data['mtn'])){
+                $data['mtn_err'] = 'Please enter MTN No';
+            }
+
+            if(!empty($data['date_err']) || !empty($data['center_err']) || !empty($data['mtn_err']) 
+               || !empty($data['error'])){
+                $this->view('stocks/addtransfer',$data);
+                exit();
+            }
+
+            if(!$this->stockmodel->CreateUpdateTransfer($data)){
+                flash('transfer_msg',null,'Transfer not created. Retry or contact admin',flashclass('alert','danger'));
+                redirect('stocks/transfers');
+                exit();
+            }
+
+            flash('transfer_toast_msg',null,'Transfer created.',flashclass('toast','success'));
+            redirect('stocks/transfers');
+            exit();
+
+        }else{
+            redirect('auth/forbidden');
+            exit();
+        }
     }
 }
