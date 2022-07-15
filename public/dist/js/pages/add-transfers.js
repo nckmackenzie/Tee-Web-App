6601ffd8cbf6adddc480713fbe511746d9a44454
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-15 19:07:33 +0300

    Set inputs to readonly

--- public/dist/js/pages/add-transfers.js
+++ public/dist/js/pages/add-transfers.js
@@ -40,8 +40,8 @@ btnadd.addEventListener('click', function () {
   let html = `
       <tr>
         <td class="d-none"><input type="text" name="booksid[]" value="${selectedValue}"></td>
-        <td><input type="text" class="table-input" name="booksname[]" value="${selectedBook}"></td>
-        <td><input type="text" class="table-input" name="qtys[]" value="${qty}"></td>
+        <td><input type="text" class="table-input" name="booksname[]" value="${selectedBook}" readonly></td>
+        <td><input type="text" class="table-input" name="qtys[]" value="${qty}" readonly></td>
         <td><button type="button" class="action-icon btn btn-sm text-danger fs-5 btndel">Remove</button></td>
       </tr>
   `;
