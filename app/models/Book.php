33bc4d2fe0994ac2782d08dc0f114bbe23bc450f
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-16 07:45:04 +0300

    Addition of centerid parameter in sp_bookslist stored procedure

--- app/models/Book.php
+++ app/models/Book.php
@@ -9,8 +9,9 @@ class Book
 
     //get books list from database
     public function GetBooks(){
-        $this->db->query('CALL sp_bookslist(:tdate)');
+        $this->db->query('CALL sp_bookslist(:tdate,:cid)');
         $this->db->bind(':tdate',date('Y-m-d'));
+        $this->db->bind(':cid',$_SESSION['centerid']);
         return $this->db->resultSet();
     }
 
