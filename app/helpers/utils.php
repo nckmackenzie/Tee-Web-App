d970e245aaad8f5349eabf5d0799591134c4ae2e
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-17 15:07:17 +0300

    Change of encrypt and decrypt functions

--- app/helpers/utils.php
+++ app/helpers/utils.php
@@ -115,51 +115,30 @@ function is_authenticated($user){
     return true;
 }
 
-function hash_equals_custom($knownString, $userString) {
-    if (function_exists('mb_strlen')) {
-        $kLen = mb_strlen($knownString, '8bit');
-        $uLen = mb_strlen($userString, '8bit');
-    } else {
-        $kLen = strlen($knownString);
-        $uLen = strlen($userString);
+function encrypt($string){
+    $key = ENCRYPTION_KEY;
+    $result = '';
+    $test = '';
+    for ($i=0; $i <strlen($string) ; $i++) { 
+        $char = substr($string, $i,1);
+        $keychar = substr($key, ($i % strlen($key))-1,1);
+        $char = chr(ord($char)+ord($keychar));
+        //$test[$char]=ord($char)+ord($keychar);
+        $result.=$char;
     }
-    if ($kLen !== $uLen) {
-        return false;
-    }
-    $result = 0;
-    for ($i = 0; $i < $kLen; $i++) {
-        $result |= (ord($knownString[$i]) ^ ord($userString[$i]));
-    }
-    return 0 === $result;
+    return urlencode(base64_encode($result));
 }
 
-function encrypt ($pure_string, $encryption_key) {
-    $cipher     = 'AES-256-CBC';
-    $options    = OPENSSL_RAW_DATA;
-    $hash_algo  = 'sha256';
-    $sha2len    = 32;
-    $ivlen = openssl_cipher_iv_length($cipher);
-    $iv = openssl_random_pseudo_bytes($ivlen);
-    $ciphertext_raw = openssl_encrypt($pure_string, $cipher, $encryption_key, $options, $iv);
-    $hmac = hash_hmac($hash_algo, $ciphertext_raw, $encryption_key, true);
-    return $iv.$hmac.$ciphertext_raw;
-}
-
-function decrypt ($encrypted_string, $encryption_key) {
-    $cipher     = 'AES-256-CBC';
-    $options    = OPENSSL_RAW_DATA;
-    $hash_algo  = 'sha256';
-    $sha2len    = 32;
-    $ivlen = openssl_cipher_iv_length($cipher);
-    $iv = substr($encrypted_string, 0, $ivlen);
-    $hmac = substr($encrypted_string, $ivlen, $sha2len);
-    $ciphertext_raw = substr($encrypted_string, $ivlen+$sha2len);
-    $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $encryption_key, $options, $iv);
-    $calcmac = hash_hmac($hash_algo, $ciphertext_raw, $encryption_key, true);
-    if(function_exists('hash_equals')) {
-        if (hash_equals($hmac, $calcmac)) return $original_plaintext;
-    } else {
-        if (hash_equals_custom($hmac, $calcmac)) return $original_plaintext;
+function decrypt($string){
+    $key = ENCRYPTION_KEY;
+    $result = '';
+    $string = base64_decode(urldecode($string));
+    for ($i=0; $i < strlen($string) ; $i++) { 
+        $char = substr($string, $i,1);
+        $keychar = substr($key, ($i % strlen($key))-1,1);
+        $char = chr(ord($char)-ord($keychar));
+        $result.=$char;
     }
+    return $result;
 }
 
