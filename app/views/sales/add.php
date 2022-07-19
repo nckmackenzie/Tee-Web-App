da2439d975aa24b45ef751322cbfd72c2bf27b23
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-19 20:36:22 +0300

    Addition message box for alerts

--- app/views/sales/add.php
+++ app/views/sales/add.php
@@ -15,6 +15,7 @@
     </div>     
     <!-- end page title --> 
     <div class="row">
+        <div class="col-12" id="message"></div>
         <div class="col-12">
             <form action="<?php echo URLROOT;?>/sales/createupdate" method="post" autocomplete="off" name="salesform">
                 <div class="card">
