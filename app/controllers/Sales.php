0520610e3bfc003608741a312e16762f1fe521da
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-19 19:30:13 +0300

    Data array changes

--- app/controllers/Sales.php
+++ app/controllers/Sales.php
@@ -21,4 +21,37 @@ class Sales extends Controller
         $this->view('sales/index',$data);
         exit();
     }
+
+    public function add()
+    {
+        $students = $this->salemodel->GetStudents();
+        $salesid = $this->salemodel->GetSaleId();
+        $books = $this->salemodel->GetBooks();
+        $data = [
+            'title' => 'Add sales',
+            'students' =>  $students,
+            'salesid' => $salesid,
+            'books' =>  $books,
+            'touched' => false,
+            'isedit' => false,
+            'id' => '',
+            'sdate' => date('Y-m-d'),
+            'student' => '',
+            'subtotal' => '',
+            'discount' => '',
+            'net' => '',
+            'paid' => '',
+            'balance' => '',
+            'table' => [],
+            'sdate' => date('Y-m-d'),
+            'student_err' => '',
+            'subtotal_err' => '',
+            'discount_err' => '',
+            'net_err' => '',
+            'paid_err' => '',
+            'balance_err' => '',
+        ];
+        $this->view('sales/add',$data);
+        exit();
+    }
 }
