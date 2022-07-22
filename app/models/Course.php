39da2039c46bd1207b44fd6f5a7f10f9544b56df
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-22 21:35:12 +0300

    Add CheckFieldAvailability and CreateUpdate methods

--- app/models/Course.php
+++ app/models/Course.php
@@ -14,4 +14,39 @@ class Course
         $this->db->query('SELECT * FROM vw_courses');
         return $this->db->resultSet();
     }
+
+    public function CheckFieldAvailability($field,$value,$id)
+    {
+        $sql = 'SELECT COUNT(*) FROM courses WHERE '.$field.' = :val AND ID <> :id';
+        $this->db->query($sql);
+        $this->db->bind(':val',strtolower($value));
+        $this->db->bind(':id',$id);
+        if(intval($this->db->getvalue()) > 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public function CreateUpdate($data)
+    {
+        if(!$data['isedit']){
+            $this->db->query('INSERT INTO courses (CourseName,CourseCode) VALUES(:cname,:code)');
+        }else{
+            $this->db->query('UPDATE courses SET CourseName=:cname,CourseCode=:code,Active = :active 
+                              WHERE (ID = :id)');
+        }
+        $this->db->bind(':cname',strtolower($data['coursename']));
+        $this->db->bind(':code',!empty($data['coursecode']) ? strtolower($data['coursecode']) : null);
+        if($data['isedit']){
+            $this->db->bind(':active',$data['active']);
+            $this->db->bind(':id',$data['id']);
+        }
+
+        if(!$this->db->execute()){
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
