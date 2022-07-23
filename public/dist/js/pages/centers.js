4dbc99dd2b421ba3e8590245152fb535ee1fe32f
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-23 12:18:17 +0300

    Refactoring datatable and delete actions

--- public/dist/js/pages/centers.js
+++ public/dist/js/pages/centers.js
@@ -1,26 +1,5 @@
-$(document).ready(function () {
-  'use strict';
-  $('#centers-datatable').DataTable({
-    language: {
-      paginate: {
-        previous: "<i class='mdi mdi-chevron-left'>",
-        next: "<i class='mdi mdi-chevron-right'>",
-      },
-      info: 'Showing Centers _START_ to _END_ of _TOTAL_',
-      lengthMenu:
-        'Display <select class=\'form-select form-select-sm ms-1 me-1\'><option value="5">5</option><option value="10">10</option><option value="20">20</option><option value="-1">All</option></select> centers',
-    },
-    pageLength: 10,
-    drawCallback: function () {
-      $('.dataTables_paginate > .pagination').addClass('pagination-rounded'),
-        $('#centers-datatable_length label').addClass('form-label');
-    },
-  });
-});
+import { datatable } from './datatable.js';
+import { btnClick } from './utils.js';
 
-function rowFunction(el, id) {
-  const input = document.querySelector('#' + id + '');
-  var n = el.parentNode.parentNode.cells[0].textContent;
-  input.value = +n;
-  // ...
-}
+datatable('centers');
+btnClick('.btndel', 'id');
