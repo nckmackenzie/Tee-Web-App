1141c50e5223b15531515547b09dcdc42671ce78
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-27 21:28:27 +0300

    Addition - createupdate method

--- app/controllers/Groups.php
+++ app/controllers/Groups.php
@@ -35,5 +35,57 @@ class Groups extends Controller
             'parishname_err' => '',
         ];
         $this->view('groups/add',$data);
+        exit();
+    }
+
+    public function createupdate()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $_POST = filter_input_array(INPUT_POST, FILTER_UNSAFE_RAW);
+            $data = [
+                'title' => converttobool(trim($_POST['isedit'])) ? 'Edit Group' : 'Add Group',
+                'id' => trim($_POST['id']),
+                'touched' => true,
+                'isedit' => converttobool(trim($_POST['isedit'])),
+                'groupname' => !empty(trim($_POST['groupname'])) ? trim($_POST['groupname']) : '',
+                'parishname' => !empty(trim($_POST['parishname'])) ? trim($_POST['parishname']) : '',
+                'active' => converttobool(trim($_POST['isedit'])) ? (isset($_POST['active']) ? true : false) : true,
+                'groupname_err' => '',
+                'parishname_err' => '',
+            ];
+
+            //validate
+            if(empty($data['groupname'])){
+                $data['groupname_err'] = 'Enter group name';
+            }
+
+            if(empty($data['parishname'])){
+                $data['parishname_err'] = 'Enter parish name';
+            }
+
+            if(!empty($data['groupname']) && !empty($data['parishname']) 
+               && !$this->groupmodel->CheckGroupName($data['groupname'],$data['parishname'])){
+                $data['groupname_err'] = 'Group name already exists';
+            }
+
+            if(!empty($data['groupname_err']) || !empty($data['parishname_err'])){
+                $this->view('groups/add',$data);
+                exit();
+            }
+
+            if(!$this->groupmodel->CreateUpdate($data)){
+                flash('group_msg',null,'Unable to save group. Contact your administrator',flashclass('alert','danger'));
+                redirect('groups');
+                exit();
+            }
+
+            flash('group_flash_msg',null,'Saved successfully!',flashclass('toast','success'));
+            redirect('groups');
+            exit();
+
+        }else{
+            redirect('auth/forbidden');
+            exit();
+        }
     }
 }
