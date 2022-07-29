af4f2f879960f1d8218416b9db8e0d2900fd87e7
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-07-29 19:25:13 +0300

    Change id for studentorgroup select

--- public/dist/js/pages/sales/studentGroupHandler.js
+++ public/dist/js/pages/sales/studentGroupHandler.js
@@ -1,6 +1,6 @@
 import { HOST_URL } from '../utils.js';
 const saleTypeSelect = document.getElementById('saletype');
-const studentSelect = document.getElementById('student');
+const studentSelect = document.getElementById('studentorgroup');
 
 saleTypeSelect.addEventListener('change', async function (e) {
   studentSelect.innerHTML = '';
