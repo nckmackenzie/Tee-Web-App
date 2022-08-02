ad1d5c58aa2f82e73e4b9fe143e7f631a8e142db
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-08-02 18:47:24 +0300

    Addition - constructor and index methods

---
+++ app/controllers/Exams.php
@@ -0,0 +1,24 @@
+<?php 
+class Exams extends Controller
+{
+    public function __construct()
+    {
+        if(!isset($_SESSION['userid'])){
+            redirect('auth');
+            exit();
+        }
+        $this->exammodel = $this->model('Exam');
+    }
+
+    public function index()
+    {
+        $exams = $this->exammodel->GetExams();
+        $data = [
+            'title' => 'Exams',
+            'has_datatable' => true,
+            'exams' => $exams
+        ];
+        $this->view('exams/index', $data);
+        exit();
+    }
+}
