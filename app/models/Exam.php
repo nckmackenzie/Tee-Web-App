20d0ee6fa5fed218f8a8cad97b756c2b80496ca6
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-08-02 19:17:22 +0300

    Addition - GetCourses method

--- app/models/Exam.php
+++ app/models/Exam.php
@@ -13,4 +13,13 @@ class Exam
         $this->db->query('SELECT * FROM vw_exams');
         return $this->db->resultSet();
     }
+
+    public function GetCourses()
+    {
+        $this->db->query('SELECT ID,UCASE(CourseName) AS CourseName 
+                          FROM courses
+                          WHERE (Deleted = 0) AND (Active = 1)
+                          ORDER BY CourseName');
+        return $this->db->resultSet();
+    }
 }
