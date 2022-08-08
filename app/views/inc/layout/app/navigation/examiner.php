0f640f26a990e2c90c7087e9b28028326dbbbdbe
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-08-08 08:37:48 +0300

    Addition of Examiners user menu

---
+++ app/views/inc/layout/app/navigation/examiner.php
@@ -0,0 +1,19 @@
+<li class="side-nav-item">
+    <a data-bs-toggle="collapse" href="#sidebarExams" aria-expanded="false" aria-controls="sidebarExams" class="side-nav-link">
+        <i class="uil-clipboard-notes"></i>
+        <span> Exams </span>
+        <span class="menu-arrow"></span>
+    </a>
+    <div class="collapse" id="sidebarExams">
+        <ul class="side-nav-second-level">
+            <li>
+                <a href="<?php echo URLROOT;?>/exams/">Exams</a>
+            </li>
+            <li>
+                <a href="<?php echo URLROOT;?>/exams/receiptmarking">Receipt For Marking</a>
+            </li>
+        </ul>
+    </div>
+</li>
+
+
