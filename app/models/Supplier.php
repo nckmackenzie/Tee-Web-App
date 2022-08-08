1ee0bfab826ffafd11259707ec1433d4af3745bd
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-08-08 21:30:38 +0300

    Addition GetSupplier method and UPDATE query addition to CreateUpdate method

--- app/models/Supplier.php
+++ app/models/Supplier.php
@@ -81,6 +81,28 @@ class Supplier
     {
         if(!$data['isedit']){
             return $this->Save($data);
+        }else{
+            $this->db->query('UPDATE suppliers SET SupplierName=:sname,Contact=:contact,`Address`=:add,
+                                                   Email=:email,PIN=:pin,ContactPerson=:cperson
+                              WHERE (ID = :id)');
+            $this->db->bind(':sname',!empty($data['suppliername']) ? strtolower($data['suppliername']) : null);
+            $this->db->bind(':contact',!empty($data['contact']) ? strtolower($data['contact']) : null);
+            $this->db->bind(':add',!empty($data['address']) ? strtolower($data['address']) : null);
+            $this->db->bind(':email',!empty($data['email']) ? $data['email'] : null);
+            $this->db->bind(':pin',!empty($data['pin']) ? strtolower($data['pin']) : null);
+            $this->db->bind(':cperson',!empty($data['contactperson']) ? strtolower($data['contactperson']) : null);
+            $this->db->bind(':id',$data['id']);
+            if(!$this->db->execute()){
+                return false;
+            }else{
+                return true;
+            }
         }
+    }
+    public function GetSupplier($id)
+    {
+        $this->db->query('SELECT * FROM suppliers WHERE (ID = :id)');
+        $this->db->bind(':id',$id);
+        return $this->db->single();
     }
 }
