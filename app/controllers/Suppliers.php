8b5db145632fa24dc0dfbbd1cc6150410f6f7bcf
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-08-08 14:28:24 +0300

    Initial class setup + constructor functions addition

---
+++ app/controllers/Suppliers.php
@@ -0,0 +1,18 @@
+<?php
+class Suppliers extends Controller 
+{
+    public function __construct()
+    {
+        if(!isset($_SESSION['userid'])){
+            redirect('auth');
+            exit();
+        }
+
+        if(!$_SESSION['ishead'] || intval($_SESSION['usertypeid']) > 2 ){
+            redirect('auth/unauthorized');
+            exit();
+        }
+
+        $this->suppliermodel = $this->model('Supplier');
+    }
+}
---
+++ app/models/Supplier.php
@@ -0,0 +1,10 @@
+<?php
+class Supplier
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+}
