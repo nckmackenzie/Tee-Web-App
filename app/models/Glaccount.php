0f2f93d121c5143b651d38f9985f0d7f76d28320
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-08-09 19:28:06 +0300

    Addition - CreateUpdate and CheckNameAvailability methods

--- app/models/Glaccount.php
+++ app/models/Glaccount.php
@@ -25,4 +25,30 @@ class Glaccount
         $this->db->query('SELECT ID,UCASE(AccountName) As AccountName FROM accounttypes WHERE AccountTypeId IS NULL');
         return $this->db->resultset();
     }
+
+    public function CheckNameAvailability($name,$id)
+    {
+        $this->db->query('SELECT COUNT(*) 
+                          FROM accounttypes 
+                          WHERE (ID <> :id) AND (AccountName = :accname)');
+        $this->db->bind(":id",$id);
+        $this->db->bind(':accname',strtolower($name));
+        if((int)$this->db->getvalue() > 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public function CreateUpdate($data)
+    {
+        $this->db->query('INSERT INTO accounttypes (AccountName,AccountTypeId) VALUEStyp,:atype)');
+        $this->db->bind(':aname',!empty($data['accountname']) ? strtolower($data['accountname']) : null);
+        $this->db->bind(':atype',!empty($data['accounttypee']) ? strtolower($data['accounttypee']) : null);
+        if(!$this->db->execute()){
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
