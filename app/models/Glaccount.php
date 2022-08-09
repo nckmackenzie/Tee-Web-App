e4fa1bec6a16e11a924173197cc15a0782e7f77b
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-08-09 17:45:22 +0300

    Addition - GetGLAccounts method

--- app/models/Glaccount.php
+++ app/models/Glaccount.php
@@ -7,4 +7,13 @@ class Glaccount
     {
         $this->db = new Database;
     }
+
+    public function GetGLAccounts()
+    {
+        $this->db->query('SELECT ID,UCASE(AccountName) AS AccountName
+                          FROM   accounttypes
+                          WHERE  (AccountTypeId IS NOT NULL) AND (IsBank = 0)
+                          ORDER BY AccountName');
+        return $this->db->resultset();
+    }
 }
