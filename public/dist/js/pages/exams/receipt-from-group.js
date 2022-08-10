d697facf20deca7631d914e3fd14db0e00724790
nicmackenzie <nicholas.mackenzie1@gmail.com>
2022-08-10 20:00:11 +0300

    JS for fetching students based on group + remove event listener

---
+++ public/dist/js/pages/exams/receipt-from-group.js
@@ -0,0 +1,18 @@
+import { HOST_URL } from '../utils.js';
+const table = document.getElementById('groupmembers');
+const groupSelect = document.getElementById('group');
+
+groupSelect.addEventListener('change', async function (e) {
+  const tbody = table.getElementsByTagName('tbody')[0];
+  tbody.innerHTML = '';
+  const groupId = +e.target.value;
+  const res = await fetch(`${HOST_URL}/exams/getstudents?gid=${groupId}`);
+  const data = await res.json();
+  tbody.innerHTML = data;
+});
+
+table.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('btndel')) return;
+  const btn = e.target;
+  btn.closest('tr').remove();
+});
