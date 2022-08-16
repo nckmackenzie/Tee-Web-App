9b44a1dc91e6ea9394dced839dbd42432709c03e
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-16 09:00:20 +0300

    bookid input and exam var declaration + exam select event change handler

--- public/dist/js/pages/exams/receipt-from-group.js
+++ public/dist/js/pages/exams/receipt-from-group.js
@@ -2,6 +2,8 @@ import { HOST_URL } from '../utils.js';
 const table = document.getElementById('groupmembers');
 const groupSelect = document.getElementById('group');
 const form = document.querySelector('form');
+const bookInput = document.getElementById('bookid');
+const examSelect = document.getElementById('exam');
 
 groupSelect.addEventListener('change', async function (e) {
   const tbody = table.getElementsByTagName('tbody')[0];
@@ -19,6 +21,14 @@ table.addEventListener('click', function (e) {
   if (!e.target.classList.contains('btndel')) return;
   const btn = e.target;
   btn.closest('tr').remove();
+});
+
+examSelect.addEventListener('change', async function (e) {
+  const examId = +e.target.value;
+  if (!examId) return;
+  const res = await fetch(`${HOST_URL}/exams/getbookid?examid=${examId}`);
+  const data = await res.json();
+  bookInput.value = data;
 });
 
 form.addEventListener('submit', function (e) {
