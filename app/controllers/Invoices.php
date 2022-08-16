c11607942bef4f4dce186db340199e92382116af
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-16 15:38:58 +0300

    Addition - add method

--- app/controllers/Invoices.php
+++ app/controllers/Invoices.php
@@ -20,4 +20,32 @@ class Invoices extends Controller
         $this->view('invoices/index', $data);
         exit();
     }
+
+    public function add()
+    {
+        $data = [
+            'title' => 'Add Invoice',
+            'suppliers' => $this->invoicemodel->GetSuppliers(),
+            'vattype' => $this->invoicemodel->GetVatTypes(),
+            'vat' => $this->invoicemodel->GetVats(),
+            'books' => $this->invoicemodel->GetBooks(),
+            'touched' => false,
+            'isedit' => '',
+            'id' => '',
+            'invoicedate' => date('Y-m-d'),
+            'duedate' => '',
+            'vattype' => '',
+            'vat' => '',
+            'invoiceno' => '',
+            'invoicedate_err' => '',
+            'duedate_err' => '',
+            'vattype_err' => '',
+            'vat_err' => '',
+            'invoiceno_err' => '',
+            'table' => [],
+            'save_err'=> ''
+        ];
+        $this->view('invoices/add', $data);
+        exit();
+    }
 }
