1b63822a728eacdb901b50a14629d5b03de1c3c0
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-16 09:38:08 +0300

    Optional rendering for mark or save errors

--- app/views/exams/receiptmarking.php
+++ app/views/exams/receiptmarking.php
@@ -14,9 +14,9 @@
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 <?php endif; ?>
-                <?php if(isset($data['save_err']) && !empty($data['save_err'])) : ?>
+                <?php if(!empty($data['mark_err']) || !empty($data['save_err'])) : ?>
                     <div class="mt-1 alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Error!</strong> <?php echo $data['save_err'];?>
+                        <strong>Error!</strong> <?php echo !empty($data['mark_err']) ? $data['mark_err'] : $data['save_err'];?>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 <?php endif; ?>
