63230443a36aa689b2453d5e7a53e95e7e16f06a
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-16 15:34:31 +0300

    Addition - GetBooks + GetVatTypes + GetVats

--- app/models/Invoice.php
+++ app/models/Invoice.php
@@ -13,4 +13,36 @@ class Invoice
         $this->db->bind(':id',(int)$_SESSION['centerid']);
         return $this->db->resultset();
     }
+
+    public function GetSuppliers()
+    {
+        $this->db->query('SELECT ID,UCASE(SupplierName) AS SupplierName 
+                          FROM suppliers 
+                          WHERE (Deleted = 0)
+                          ORDER BY SupplierName');
+        return $this->db->resultset();
+    }
+
+    public function GetBooks()
+    {
+        $this->db->query('SELECT ID,UCASE(Title) AS BookName 
+                          FROM books 
+                          WHERE (Deleted = 0) AND (Active =1)
+                          ORDER BY BookName');
+        return $this->db->resultset();
+    }
+
+    public function GetVatTypes()
+    {
+        $this->db->query('SELECT ID,UCASE(VatType) AS VatType 
+                          FROM vat_types');
+        return $this->db->resultset();
+    }
+
+    public function GetVats()
+    {
+        $this->db->query('SELECT ID,UCASE(vat) AS Vat
+                          FROM vat');
+        return $this->db->resultset();
+    }
 }
