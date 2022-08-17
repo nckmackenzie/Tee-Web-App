80489ecdc256983df075d398c862755f2ea57d53
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-17 13:46:33 +0300

    Vat var decl and vat type select change handler

--- public/dist/js/pages/invoices/add-invoice.js
+++ public/dist/js/pages/invoices/add-invoice.js
@@ -1,5 +1,7 @@
 const invoiceDateInput = document.querySelector('#invoicedate');
 const dueDateInput = document.querySelector('#duedate');
+const vatTypeSelect = document.querySelector('#vattype');
+const vatSelect = document.querySelector('#vat');
 
 invoiceDateInput.addEventListener('change', function (e) {
   const currDate = new Date(e.target.value);
@@ -11,4 +13,15 @@ invoiceDateInput.addEventListener('change', function (e) {
     '-' +
     ('0' + newDate.getDate()).slice(-2);
   dueDateInput.value = formatedDate;
+});
+
+vatTypeSelect.addEventListener('change', function (e) {
+  const vatType = +e.target.value;
+  if (!vatType) return;
+  if (vatType === 1) {
+    vatSelect.disabled = true;
+  } else {
+    vatSelect.disabled = false;
+  }
+  vatSelect.value = '';
 });
