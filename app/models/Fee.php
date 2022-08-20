ac8ca828db72963fc7183cf11902a0f24a0cac35
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-20 12:22:25 +0300

    Addition - CheckRefExists,GetAccountDetails,CreateUpdate methods

--- app/models/Fee.php
+++ app/models/Fee.php
@@ -44,4 +44,91 @@ class Fee
     {
         return getuniqueid($this->db->dbh,'ReceiptNo','fees_payment',(int)$_SESSION['centerid']);
     }
+
+    public function CheckRefExists($ref,$id)
+    {
+        $this->db->query('SELECT 
+                            COUNT(*)
+                          FROM 
+                            fees_payment 
+                          WHERE 
+                            (Reference = :ref) AND (ID <> :id) AND (Deleted = 0)');
+        $this->db->bind(':ref',strtolower($ref));
+        $this->db->bind(':id',$id);
+        if((int)$this->db->getvalue() > 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    function GetAccountDetails($id){
+        $this->db->query('SELECT AccountName,AccountTypeId FROM accounttypes WHERE ID = :id');
+        $this->db->bind(':id',(int)$id);
+        $account = $this->db->single();
+        return [$account->AccountName,$account->AccountTypeId];
+    }
+
+    public function CreateUpdate($data)
+    {
+        try {
+            $this->db->dbh->beginTransaction();
+
+            if(!$data['isedit']){
+                $sql = 'INSERT INTO fees_payment (PaymentDate,ReceiptNo,StudentId,AmountPaid,GlAccountId,
+                                                  PaymentMethodId,Reference,Narration,CenterId) 
+                        VALUES(:pdate,:receipt,:student,:amount,:account,:paymethod,:reference,:narr,:cid)';
+            }else{
+                $sql = 'UPDATE fees_payment SET PaymentDate=:pdate,ReceiptNo=:receipt,StudentId=:student,AmountPaid
+                                                ,GlAccountId=:account,PaymentMethodId=:paymethod,Reference=:reference
+                                                ,Narration=:narr 
+                        WHERE (ID = :id)';
+            }
+            $this->db->query($sql);
+            $this->db->bind(':pdate',!empty($data['pdate']) ? $data['pdate'] : null);
+            $this->db->bind(':receipt',!empty($data['receiptno']) ? $data['receiptno'] : null);
+            $this->db->bind(':student',!empty($data['student']) ? $data['student'] : null);
+            $this->db->bind(':amount',!empty($data['amount']) ? $data['amount'] : null);
+            $this->db->bind(':account',!empty($data['account']) ? $data['account'] : null);
+            $this->db->bind(':paymethod',!empty($data['paymethod']) ? $data['paymethod'] : null);
+            $this->db->bind(':reference',!empty($data['reference']) ? strtolower($data['reference']) : null);
+            $this->db->bind(':narr',!empty($data['narration']) ? strtolower($data['narration']) : null);
+            if(!$data['isedit']){
+                $this->db->bind(':cid',$_SESSION['centerid']);
+            }else{
+                $this->db->bind(':id',$data['id']);
+            }
+            $this->db->execute();
+            $tid = !$data['isedit'] ? $this->db->dbh->lastInsertId() : $data['id'];
+
+            if($data['isedit']){
+                $this->db->query('DELETE FROM ledger WHERE TransactionType = 5 AND TransactionId = :id');
+                $this->db->bind(':id',$data['id']);
+                $this->db->execute(); 
+            }
+
+            savetoledger($this->db->dbh,$data['pdate'],$this->GetAccountDetails($data['account'])[0],0,$data['amount'],
+                         strtolower($data['narration']),$this->GetAccountDetails($data['account'])[1],5,$tid,$_SESSION['centerid']);
+            if((int)$data['paymethod'] === 1){
+                savetoledger($this->db->dbh,$data['pdate'],'cash at hand',$data['amount'],0,
+                         strtolower($data['narration']),3,5,$tid,$_SESSION['centerid']);
+            }else{
+                savetoledger($this->db->dbh,$data['pdate'],'cash at bank',$data['amount'],0,
+                         strtolower($data['narration']),3,5,$tid,$_SESSION['centerid']);
+            }
+
+            if(!$this->db->dbh->commit()){
+                return false;
+            }else{
+                return true;
+            }
+            
+        } catch (\Exception $e) {
+            if(!$this->db->dbh->inTransaction()){
+                $this->db->dbh->rollback();
+            }
+            throw $e;
+            return false;
+        }
+    }
 }
