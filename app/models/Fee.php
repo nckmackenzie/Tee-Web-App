5fdeb7dad7068e448a9105456e2d8f374090c600
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-20 10:49:09 +0300

    GetAccounts,GetReceiptNo,GetStudents methods addition

--- app/models/Fee.php
+++ app/models/Fee.php
@@ -13,4 +13,35 @@ class Fee
         $this->db->bind(':cid',(int)$_SESSION['centerid']);
         return $this->db->resultset();
     }
+
+    public function GetStudents()
+    {
+        $this->db->query('SELECT 
+                            ID,
+                            UCASE(StudentName) As StudentName 
+                          FROM 
+                            students 
+                          WHERE 
+                            Deleted = 0
+                          ORDER BY StudentName');
+        return $this->db->resultset();
+    }
+
+    public function GetAccounts()
+    {
+        $this->db->query('SELECT 
+                            ID,
+                            UCASE(AccountName) As AccountName 
+                          FROM 
+                            accounttypes 
+                          WHERE 
+                            AccountTypeId = 1
+                          ORDER BY AccountName');
+        return $this->db->resultset();
+    }
+
+    public function GetReceiptNo()
+    {
+        return getuniqueid($this->db->dbh,'ReceiptNo','fees_payment',(int)$_SESSION['centerid']);
+    }
 }
