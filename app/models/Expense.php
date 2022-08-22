cd9f5eeaa8ab8c3a71df72254c096aab8b6956b2
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-22 13:06:24 +0300

    Addition - GetExpenseAccounts method

--- app/models/Expense.php
+++ app/models/Expense.php
@@ -13,4 +13,17 @@ class Expense
         $this->db->bind(':cid',(int)$_SESSION['centerid']);
         return $this->db->resultset();
     }
+
+    public function GetExpenseAccounts()
+    {
+        $this->db->query('SELECT
+                            ID,
+                            UCASE(AccountName) AS AccountName
+                          FROM 
+                            accounttypes
+                          WHERE 
+                            AccountTypeId = 2
+                          ORDER BY AccountName');
+        return $this->db->resultset();
+    }
 }
