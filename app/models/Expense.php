140b7ddd5bbc1314b1589fdc8ad5b0a58c7580f7
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-22 11:58:49 +0300

    Addition - GetExpenses method

--- app/models/Expense.php
+++ app/models/Expense.php
@@ -6,4 +6,11 @@ class Expense
     {
        $this->db = new Database;       
     }
+
+    public function GetExpenses()
+    {
+        $this->db->query('SELECT * FROM vw_expenses WHERE CenterId = :cid');
+        $this->db->bind(':cid',(int)$_SESSION['centerid']);
+        return $this->db->resultset();
+    }
 }
