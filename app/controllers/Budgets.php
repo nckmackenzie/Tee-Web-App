2fc5d5ac201ed13ee0fb82e85c29438215418ba2
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-23 08:48:57 +0300

    Constructor setup

---
+++ app/controllers/Budgets.php
@@ -0,0 +1,15 @@
+<?php
+class Budgets extends Controller
+{
+    public function __construct()
+    {
+        if(!isset($_SESSION['userid'])){
+            redirect('auth');
+            exit();
+        }
+        if((int)$_SESSION['usertypeid'] > 3){
+            redirect('auth/unauthorized');
+            exit();
+        }
+    }
+}
