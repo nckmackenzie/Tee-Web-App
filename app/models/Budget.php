27b8c748860902793df51f142dd812d77474194b
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-23 12:30:09 +0300

    Addition - GetExpenseAccounts,GetOpenYears methods

--- app/models/Budget.php
+++ app/models/Budget.php
@@ -13,4 +13,29 @@ class Budget
         $this->db->bind(':cid',(int)$_SESSION['centerid']);
         return $this->db->resultset();
     }
+
+    public function GetOpenYears()
+    {
+        $this->db->query('SELECT 
+                            ID,
+                            UCASE(YearName) AS YearName
+                          FROM
+                            years
+                          WHERE
+                            closed = 0 AND Deleted = 0');
+        return $this->db->resultset();
+    }
+
+    public function GetExpenseAccounts()
+    {
+        $this->db->query('SELECT 
+                            ID,
+                            UCASE(AccountName) AS AccountName
+                          FROM
+                            accounttypes
+                          WHERE
+                            AccountTypeId = 2
+                          ORDER BY AccountName');
+        return $this->db->resultset();
+    }
 }
