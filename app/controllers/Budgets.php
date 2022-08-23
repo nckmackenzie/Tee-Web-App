5e580f68c827bfce0c176a1be4ff6b072e569cda
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-23 12:30:48 +0300

    Addition - add method

--- app/controllers/Budgets.php
+++ app/controllers/Budgets.php
@@ -24,4 +24,31 @@ class Budgets extends Controller
         $this->view('budgets/index',$data);
         exit();
     }
+
+    public function add()
+    {
+        $data = [
+            'title' => 'Add Budget',
+            'years' => $this->budgetmodel->GetOpenYears(),
+            'glaccounts' => $this->budgetmodel->GetExpenseAccounts(),
+            'id' => '',
+            'isedit' => false,
+            'touched' => false,
+            'budgetname' => '',
+            'year' => '',
+            'table' => [],
+            'budgetname_err' => '',
+            'year_err' => '',
+            'save_err' => '',
+        ];
+        foreach($data['glaccounts'] as $account){
+            array_push($data['table'],[
+                'aid' => $account->ID,
+                'name' => $account->AccountName,
+                'amount' => ''
+            ]);
+        }
+        $this->view('budgets/add',$data);
+        exit();
+    }
 }
