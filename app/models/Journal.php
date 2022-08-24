ba8318bbcade30a0fda8d08adee0523cf22829c6
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-24 10:16:39 +0300

    Addition - GetGlAccounts method

--- app/models/Journal.php
+++ app/models/Journal.php
@@ -6,4 +6,17 @@ class Journal
     {
         $this->db = new Database;
     }
+
+    public function GetGlAccounts()
+    {
+        $this->db->query('SELECT 
+                            ID,
+                            UCASE(AccountName) As AccountName
+                          FROM
+                            accounttypes
+                          WHERE
+                            AccountTypeId IS NOT NULL AND (IsBank = 0)
+                          ORDER BY AccountName ASC');
+        return $this->db->resultset();
+    }
 }
