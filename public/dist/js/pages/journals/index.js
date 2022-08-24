9db2d747f6a9a23609b97b19ae557b62e462b30f
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-24 11:56:06 +0300

    Add to table functionalities

---
+++ public/dist/js/pages/journals/index.js
@@ -0,0 +1,98 @@
+import { getSelectedText, dateFormat } from '../utils.js';
+const jdateInput = document.getElementById('jdate');
+const accountSelect = document.getElementById('account');
+const typeSelect = document.getElementById('type');
+const amountInput = document.getElementById('amount');
+const jdateSpan = document.getElementById('jdate_span');
+const accountSpan = document.getElementById('account_span');
+const typeSpan = document.getElementById('type_span');
+const amountSpan = document.getElementById('amount_span');
+const addBtn = document.getElementById('addbtn');
+const table = document.getElementById('details');
+
+function singleValidate(elm, msg, span, span_err) {
+  if (!elm.value) {
+    span_err = msg;
+    span.textContent = span_err;
+    elm.classList.add('is-invalid');
+  } else {
+    span_err = '';
+    span.textContent = span_err;
+    elm.classList.remove('is-invalid');
+  }
+}
+
+function validate() {
+  let jdate_err = '';
+  let account_err = '';
+  let type_err = '';
+  let amount_err = '';
+
+  singleValidate(jdateInput, 'Please enter a valid date', jdateSpan, jdate_err);
+  singleValidate(
+    accountSelect,
+    'Please select account.',
+    accountSpan,
+    account_err
+  );
+  singleValidate(typeSelect, 'Please select debit/credit.', typeSpan, type_err);
+  singleValidate(amountInput, 'Please enter amount.', amountSpan, amount_err);
+
+  if (
+    jdate_err !== '' ||
+    account_err !== '' ||
+    amount_err !== '' ||
+    type_err !== ''
+  ) {
+    return false;
+  } else if (
+    jdate_err === '' &&
+    account_err === '' &&
+    amount_err === '' &&
+    type_err === ''
+  ) {
+    return true;
+  }
+}
+
+function clear() {
+  jdateInput.value =
+    accountSelect.value =
+    typeSelect.value =
+    amountInput.value =
+      '';
+}
+
+function appendToTable() {
+  const accountName = getSelectedText(accountSelect);
+  const accountValue = accountSelect.value;
+  const jdateValue = dateFormat(jdateInput.value);
+  const typeName = getSelectedText(typeSelect);
+  const amountValue = amountInput.value;
+  const body = table.getElementsByTagName('tbody')[0];
+
+  let html = `
+      <tr>
+        <td style="width:15%"><input type="text" class="table-input" name="jdates[]" value="${jdateValue}" readonly></td>
+        <td class="d-none"><input type="text" name="accountsid[]" value="${accountValue}" readonly></td>
+        <td><input type="text" class="table-input w-100" name="accountsname[]" value="${accountName}" readonly></td>
+        <td style="width:15%"><input type="text" class="table-input" name="types[]" value="${typeName}" readonly></td>
+        <td style="width:15%"><input type="text" class="table-input" name="gross[]" value="${amountValue}" readonly></td>
+        <td style="width:10%"><button type="button" class="action-icon btn btn-sm text-danger fs-5 btndel">Remove</button></td>
+      </tr>
+  `;
+  let newRow = body.insertRow(body.rows.length);
+  newRow.innerHTML = html;
+  clear();
+}
+
+addBtn.addEventListener('click', function () {
+  if (!validate()) return;
+  appendToTable();
+});
+
+table.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('btndel')) return;
+  const btn = e.target;
+  btn.closest('tr').remove();
+});
