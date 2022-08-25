56d75b894c059d9f7b3e32c7273cd8b286d01a9e
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-08-25 10:51:53 +0300

    set id attribute to isedit hidden input

--- app/views/journals/index.php
+++ app/views/journals/index.php
@@ -129,7 +129,7 @@
                             </div>
                             <div class="d-grid d-md-block">
                                 <input type="hidden" name="id" value="<?php echo $data['id'];?>">
-                                <input type="hidden" name="isedit" value="<?php echo $data['isedit'];?>">
+                                <input type="hidden" name="isedit" id="isedit" value="<?php echo $data['isedit'];?>">
                                 <input type="hidden" name="isfirst" value="<?php echo $data['isfirst'];?>">
                                 <button class="btn btn-primary" type="submit"> Save </button>            
                             </div>
