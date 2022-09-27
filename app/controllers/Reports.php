140f6483d327f19fb2c7f2ae98e744bc391de9f6
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-09-27 09:10:44 +0300

    setup feepayments,feepaymentsrpt and constructor methods

---
+++ app/controllers/Reports.php
@@ -0,0 +1,48 @@
+<?php
+class Reports extends Controller
+{
+    public function __construct()
+    {
+        if(!isset($_SESSION['userid'])):
+            redirect('auth');
+            exit();
+        endif;
+
+        $this->reportmodel = $this->model('Report');
+    }
+
+    public function feepayments()
+    {
+        $data = [
+            'title' => 'Fee Payment Report',
+            'has_datatable' => true,
+        ];
+        $this->view('reports/feepayments',$data);
+        exit;
+    }
+    public function feepaymentsrpt()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+            $_GET = filter_input_array(INPUT_GET,FILTER_UNSAFE_RAW);
+            $data = [
+                'sdate' => date('Y-m-d',strtotime($_GET['sdate'])),
+                'edate' => date('Y-m-d',strtotime($_GET['edate'])),
+                'results' => []
+            ];
+            $results = $this->reportmodel->Getfeepayments($data);
+            foreach ($results as $result):
+                array_push($data['results'],[
+                    'paymentDate' => $result->PaymentDate,
+                    'receiptNo' => $result->ReceiptNo,
+                    'studentName' => $result->StudentName,
+                    'amount' => $result->AmountPaid,
+                    'paymentReference' => $result->PaymentReference
+                ]);
+            endforeach;
+            echo json_encode($data['results']);
+        }else{
+            redirect('auth/forbidden');
+            exit();
+        }
+    }
+}
