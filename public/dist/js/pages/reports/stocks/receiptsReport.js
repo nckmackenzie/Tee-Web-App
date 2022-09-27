5ceba1c6b5bcf89e3de2031c3c6897ca6472ed58
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-09-27 15:42:27 +0300

    initial setup

--- public/dist/js/pages/reports/stocks/receiptsReport.js
+++ public/dist/js/pages/reports/stocks/receiptsReport.js
@@ -4,7 +4,7 @@ const start = document.querySelector('#start');
 const startspan = document.querySelector('.startspan');
 const endspan = document.querySelector('.endspan');
 const end = document.querySelector('#end');
-const type = document.querySelector('#type');
+const typeSelect = document.querySelector('#type');
 const preview = document.querySelector('.preview');
 const results = document.querySelector('#results');
 
@@ -12,8 +12,9 @@ preview.addEventListener('click', async () => {
   if (!validatedate(start, end, startspan, endspan)) return;
   const sdate = start.value;
   const edate = end.value;
+  const typeValue = +typeSelect.value;
   const res = await fetch(
-    `${HOST_URL}/reports/feepaymentsrpt?sdate=${sdate}&edate=${edate}`
+    `${HOST_URL}/stockreports/receiptsrpt?sdate=${sdate}&edate=${edate}`
   );
   const data = await res.json();
   let table = `
