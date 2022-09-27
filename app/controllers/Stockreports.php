5a61f2d37ce5ba38cc5deac6ea0dea687694b8d4
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-09-27 19:42:02 +0300

    Add receiptsrpt method

--- app/controllers/Stockreports.php
+++ app/controllers/Stockreports.php
@@ -21,4 +21,34 @@ class Stockreports extends Controller
         $this->view('stockreports/receipts', $data);
         exit;
     }
+
+    public function receiptsrpt()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+            $_GET = filter_input_array(INPUT_GET,FILTER_UNSAFE_RAW);
+            $data = [
+                'sdate' => date('Y-m-d',strtotime($_GET['sdate'])),
+                'edate' => date('Y-m-d',strtotime($_GET['edate'])),
+                'type' => trim($_GET['type']),
+                'results' => []
+            ];
+
+            $results = $this->reportmodel->GetReceipts($data);
+            foreach($results as $result):
+                array_push($data['results'],[
+                    'receiptDate' => $result->ReceiptDate,
+                    'grnNo' => $result->GrnNo,
+                    'receiptType' => $result->ReceiptType,
+                    'fromCenter' => strtoupper($result->Center),
+                    'book' => strtoupper($result->Title),
+                    'qty' => $result->Qty
+                ]);
+            endforeach;
+
+            echo json_encode($data['results']);
+        }else{
+            redirect('auth/forbidden');
+            exit;;
+        }
+    }
 }
