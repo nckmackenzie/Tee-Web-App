b0437ad36eee819144c7b0945394c5549ae7aefa
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-09-27 19:43:10 +0300

    add GetReceipts method

--- app/models/Stockreport.php
+++ app/models/Stockreport.php
@@ -6,4 +6,14 @@ class Stockreport
     {
         $this->db = new Database;
     }
+    //get receipts
+    public function GetReceipts($data)
+    {
+        $this->db->query('CALL sp_get_receipts(:sdate,:edate,:rtype,:cid)');
+        $this->db->bind(':sdate',$data['sdate']);
+        $this->db->bind(':edate',$data['edate']);
+        $this->db->bind(':rtype',(int)$data['type']);
+        $this->db->bind(':cid',(int)$_SESSION['centerid']);
+        return $this->db->resultSet();
+    }
 }
