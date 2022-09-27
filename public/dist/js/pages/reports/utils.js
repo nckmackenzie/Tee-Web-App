7726d4ca4d424e1827c975725755177512919071
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-09-27 11:01:56 +0300

    add updateColumnTotal function

--- public/dist/js/pages/reports/utils.js
+++ public/dist/js/pages/reports/utils.js
@@ -17,3 +17,40 @@ export function validatedate(startEl, endEl, startSpan, endSpan) {
   if (startSpan.textContent !== '' || endSpan.textContent !== '') return false;
   return true;
 }
+
+export function setdatatable(tbl) {
+  $(document).ready(function () {
+    'use strict';
+    var table = $(`#${tbl}`).DataTable();
+    table.destroy();
+    table = $(`#${tbl}`)
+      .DataTable({
+        lengthChange: !1,
+        buttons: ['copy', 'print'],
+        language: {
+          paginate: {
+            previous: "<i class='mdi mdi-chevron-left'>",
+            next: "<i class='mdi mdi-chevron-right'>",
+          },
+        },
+        drawCallback: function () {
+          $('.dataTables_paginate > .pagination').addClass(
+            'pagination-rounded'
+          );
+        },
+      })
+      .buttons()
+      .container()
+      .appendTo(`#${tbl}_wrapper .col-md-6:eq(0)`);
+  });
+}
+
+export function updateColumnTotal(tbl, i, col) {
+  var mytable = document.getElementById(tbl).getElementsByTagName('tbody')[0]; //
+  let subTotal = Array.from(mytable.rows)
+    .slice(0)
+    .reduce((total, row) => {
+      return total + parseFloat(row.cells[i].innerHTML);
+    }, 0);
+  document.getElementById(col).innerHTML = subTotal.toFixed(2);
+}
