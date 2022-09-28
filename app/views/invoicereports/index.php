c33b1b6ea7e8c25ccd5cd3f7219f7a1f29fbb49f
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-09-28 10:57:14 +0300

    renamed from due.php to index.php

--- app/views/invoicereports/index.php
+++ app/views/invoicereports/index.php
@@ -15,22 +15,33 @@
     <div class="row mb-1">
         <div class="col-md-3 sm-m-3">
             <label for="type">Report Type</label>
-            <select name="type" id="type" class="form-select form-select-sm">
+            <select name="type" id="type" class="control form-select form-select-sm">
                 <option value="" selected disabled>Select report</option>
                 <option value="due">Due Invoices</option>
                 <option value="balances">Invoices with balances</option>
                 <option value="bydate">Invoices by date</option>
                 <option value="bysupplier">Invoices by supplier</option>
             </select>
+            <span class="invalid-feedback typespan"></span>
+        </div>
+        <div class="col-md-3 sm-m-3">
+            <label for="supplier">Suppliers</label>
+            <select name="supplier" id="supplier" class="control form-select form-select-sm" disabled>
+                <option value="" selected disabled>Select supplier</option>
+                <?php foreach($data['suppliers'] as $supplier) : ?>
+                    <option value="<?php echo $supplier->ID;?>"><?php echo $supplier->SupplierName;?></option>
+                <?php endforeach; ?>
+            </select>
+            <span class="invalid-feedback supplierspan"></span>
         </div>
         <div class="col-md-3 sm-m-3">
             <label for="start">Start Date</label>
-            <input type="date" name="start" id="start" class="form-control form-control-sm" disabled>
-            <span class="invalid-feedback endspan"></span>
+            <input type="date" name="start" id="start" class="control form-control form-control-sm" disabled>
+            <span class="invalid-feedback startspan"></span>
         </div>
         <div class="col-md-3 sm-m-3">
             <label for="end">End Date</label>
-            <input type="date" name="end" id="end" class="form-control form-control-sm" disabled>
+            <input type="date" name="end" id="end" class="control form-control form-control-sm" disabled>
             <span class="invalid-feedback endspan"></span>
         </div>
     </div> 
@@ -45,5 +56,6 @@
         </div>
     </div>                     
 </div> <!-- container -->
-<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?>                    
+<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?>  
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/reports/invoices/dueInvoices.js"></script>                  
 <?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
