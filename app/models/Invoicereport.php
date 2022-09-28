b2b169e7487fb7526b33f9e3377c359b0c2dda99
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-09-28 10:59:12 +0300

    add GetDue method

--- app/models/Invoicereport.php
+++ app/models/Invoicereport.php
@@ -13,4 +13,11 @@ class Invoicereport
         $this->db->query('SELECT ID,UCASE(SupplierName) As SupplierName FROM suppliers WHERE (Deleted = 0)');
         return $this->db->resultset();
     }
+
+    public function GetDue()
+    {
+        $this->db->query('CALL sp_get_due_invoices(:cid)');
+        $this->db->bind(':cid',(int)$_SESSION['centerid']);
+        return $this->db->resultset();
+    }
 }
