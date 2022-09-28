c95937fd220715bc69db988c33ed4ee6eae0c859
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-09-28 11:00:00 +0300

    change methods name from due to index, added duerpt method

--- app/controllers/Invoicereports.php
+++ app/controllers/Invoicereports.php
@@ -15,21 +15,34 @@ class Invoicereports extends Controller
         $this->reportmodel = $this->model('Invoicereport');
     }
 
-    public function due()
+    public function index()
     {
         $data = [
-            'title' => 'Due Invoices',
+            'title' => 'Invoices Reports',
             'has_datatable' => true,
             'suppliers' => $this->reportmodel->GetSuppliers()
         ];
-        $this->view('invoicereports/due', $data);
+        $this->view('invoicereports/index', $data);
         exit;
     }
 
     public function duerpt()
     {
         if($_SERVER['REQUEST_METHOD'] === 'GET'){
-
+            $results = $this->reportmodel->GetDue();
+            $data = [];
+            foreach($results as $result){
+                array_push($data,[
+                    'invoiceDate' => $result->InvoiceDate,
+                    'dueDate' => $result->DueDate,
+                    'supplierName' => $result->SupplierName,
+                    'invoiceNo' => $result->InvoiceNo,
+                    'invoiceValue' => $result->InclusiveVat,
+                    'amountPaid' => $result->AmountPaid,
+                    'balance' => $result->Balance
+                ]);
+            }
+            echo json_encode($data);
         }else{
             redirect('auth/forbidden');
             exit;
