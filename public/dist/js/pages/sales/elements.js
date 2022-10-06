ee26f443b78c0bbccadb44b41dd8cbce37e31d8a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-06 20:50:44 +0300

    remove summary exports

--- public/dist/js/pages/sales/elements.js
+++ public/dist/js/pages/sales/elements.js
@@ -1,14 +1,38 @@
+import {
+  subtotalInput,
+  discountInput,
+  netInput,
+  paidInput,
+  balanceInput,
+} from './calculations.js';
 export const saleTypeSelect = document.getElementById('saletype');
 export const studentSelect = document.getElementById('studentorgroup');
 export const studentTable = document.getElementById('studentList');
 export const selectAllBtn = document.getElementById('selectAll');
+export const paymethodSelect = document.getElementById('paymethod');
+export const referenceInput = document.getElementById('reference');
 export const table = document.getElementById('addsale');
 export const addBtn = document.querySelector('.btnadd');
 export const bookSelect = document.getElementById('book');
 export const stockInput = document.getElementById('stock');
 export const selectedDate = document.getElementById('sdate');
+export const payDate = document.getElementById('sdate');
 export const alertBox = document.getElementById('message');
 export const form = document.querySelector('form');
 export const mandatoryFields = document.querySelectorAll('.mandatory');
-export const subtotalInput = document.querySelectorAll('.subtotal');
-export const paidInput = document.querySelectorAll('.paid');
+
+export function headerDetails() {
+  return {
+    sdate: selectedDate.value,
+    pdate: payDate.value,
+    saleType: saleTypeSelect.value,
+    buyer: studentSelect.value,
+    paymethod: paymethodSelect.value,
+    reference: referenceInput.value,
+    subtotal: subtotalInput.value,
+    discount: discountInput.value,
+    net: netInput.value,
+    paid: paidInput.value,
+    balance: balanceInput.value,
+  };
+}
