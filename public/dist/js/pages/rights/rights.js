d908a48eb22135de72c637badf4eb9bd2ccac3cd
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-09 12:39:20 +0300

    form submit event

--- public/dist/js/pages/rights/rights.js
+++ public/dist/js/pages/rights/rights.js
@@ -1,4 +1,10 @@
-import { sendHttpRequest, displayAlert, HOST_URL } from '../utils.js';
+import {
+  sendHttpRequest,
+  displayAlert,
+  HOST_URL,
+  setLoadingState,
+  resetLoadingState,
+} from '../utils.js';
 const form = document.getElementById('rights-form');
 const userSelect = document.getElementById('user');
 const table = document.getElementById('rights');
@@ -6,6 +12,7 @@ const span = document.querySelector('.invalid-feedback');
 const spinnerContainer = document.querySelector('.spinner-container');
 const tableContainer = document.querySelector('.rights-container');
 const alertBox = document.getElementById('alertBox');
+const btn = document.querySelector('.btn-block');
 let user;
 // const checkBox = document.querySelectorAll('access');
 
@@ -26,12 +33,6 @@ userSelect.addEventListener('change', async function (e) {
   }
 });
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  document.form.submit();
-});
-
 function setLoadingSpinner() {
   tableContainer.querySelector('.card').classList.add('d-none');
   table.getElementsByTagName('tbody')[0].innerHTML = '';
@@ -49,7 +50,7 @@ function setTable(forms) {
             <td>${form.Module}</td>
             <td>
               <div class="form-check mb-2">
-                <input type="checkbox" name="active" class="form-check-input" id="active" ${
+                <input type="checkbox" name="active" class="form-check-input" ${
                   +form.access === 1 && 'checked'
                 }>
               </div>
@@ -64,4 +65,40 @@ function removeLoadingSpinner() {
   tableContainer.querySelector('.card').classList.remove('d-none');
   table.getElementsByTagName('tbody')[0].innerHTML = '';
   spinnerContainer.innerHTML = '';
+}
+
+function tableData() {
+  const trs = table.getElementsByTagName('tbody')[0].querySelectorAll('tr');
+  const tableData = [];
+  trs.forEach(tr => {
+    const formId = tr.querySelector('.formid').textContent;
+    const access = tr.querySelector('.form-check-input').checked;
+    tableData.push({ formId, access });
+  });
+  return tableData;
+}
+
+form.addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  const formData = { user: user, tableData: tableData() };
+  setLoadingState(btn, 'Saving...');
+  const response = await sendHttpRequest(
+    `${HOST_URL}/userrights/createupdate`,
+    'POST',
+    JSON.stringify(formData),
+    { 'Content-Type': 'application/json' },
+    alertBox
+  );
+  resetLoadingState(btn, 'Save');
+  if (response?.success) {
+    displayAlert(alertBox, 'Rights assigned successfully', 'success');
+    resetWindow();
+  }
+});
+
+function resetWindow() {
+  userSelect.value = '';
+  tableContainer.querySelector('.card').classList.add('d-none');
+  table.getElementsByTagName('tbody')[0].innerHTML = '';
 }
