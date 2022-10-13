3c7bc258e9c9ef3693914626d56e163d273edf9c
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-13 10:43:46 +0300

    functionality to change center from home page

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,10 +5,43 @@ class Home extends Controller {
         if(!isset($_SESSION['userid'])){
             redirect('auth');
         }
+        $this->reusemodel = $this->model('Reusable');
     }
+
     public function index()
     {
-        $data = ['title' => 'Dashboard'];
+        $data = [
+           'title' => 'Dashboard',
+           'centers' => $this->reusemodel->GetCenters()
+        ];
         $this->view('home/index', $data);
+    }
+
+    public function changecenter()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $centerid = htmlentities(trim($_POST['center']));
+            $centerdetails = $this->reusemodel->GetCenterDetails($centerid);
+            $centername = strtoupper($centerdetails->CenterName);
+            $ishead = converttobool($centerdetails->IsHead);
+            $isexam = converttobool($centerdetails->ExamCenter);
+
+            unset($_SESSION['centerid']);
+            unset($_SESSION['centername']);
+            unset($_SESSION['ishead']);
+            unset($_SESSION['examcenter']);
+
+            $_SESSION['centerid'] = $centerid;
+            $_SESSION['centername'] = $centername;
+            $_SESSION['ishead'] = $ishead;
+            $_SESSION['examcenter'] = $isexam;
+
+            flash('home_msg',null,'Successfully changed center to '.ucwords($centername) ,flashclass('alert','success'));
+            redirect('home');
+
+        }else{
+            redirect('auth/forbidden');
+            exit;
+        }
     }
 }
