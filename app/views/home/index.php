1e68fbe69a26e84560356f9e3392eeb8efee84da
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-13 10:44:12 +0300

    render form to allow for change of center

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,13 +4,14 @@
 <div class="container-fluid">
     <!-- start page title -->
     <div class="row">
-        <div class="col-12">
+        <div class="col-md-6">
             <div class="page-title-box">
                 <div class="page-title-right"></div>
                 <h4 class="page-title">Dashboard</h4>
             </div>
         </div>
-    </div>     
+    </div> 
+    <?php flash('home_msg','alert');?>    
     <!-- end page title --> 
    <?php if(ENV === 'development') : ?>
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -18,6 +19,24 @@
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
    <?php endif; ?>
+   <?php if((int)$_SESSION['usertypeid'] === 1)  : ?>
+        <form action="<?php echo URLROOT;?>/home/changecenter" method="POST">
+            <div class="row">
+                <div class="col-md-4 mb-2">
+                    <label for="center">Change Center</label>
+                    <select name="center" id="center" class="form-select form-select-sm">
+                        <option value="">Select center...</option>
+                        <?php foreach($data['centers'] as $center) : ?>
+                            <option value="<?php echo $center->ID;?>"><?php echo $center->CenterName;?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            </div>
+            <div class="d-grid d-md-block">
+                <button type="submit" class="btn btn-sm btn-primary">Change Center</button>
+            </div>
+        </form>
+    <?php endif; ?>
 </div> <!-- container -->
 <?php require APPROOT .'/views/inc/layout/app/footer.php'; ?>                    
 <?php flash('home_msg','toast');?>
