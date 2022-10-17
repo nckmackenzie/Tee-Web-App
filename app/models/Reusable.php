3aa5e136ba35a63c16ae9f9e2191494ac75e33e3
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-17 08:54:01 +0300

    add GetAuthorizedCenter method to filter center authorized for logged in user

--- app/models/Reusable.php
+++ app/models/Reusable.php
@@ -16,6 +16,17 @@ class Reusable
         return loadresultset($this->db->dbh,$sql,[]);
     }
 
+    public function GetAuthorizedCenters()
+    {
+        if((int)$_SESSION['usertypeid'] === 1){
+            return $this->GetCenters();
+        }else{
+            $sql = 'SELECT u.CenterId AS ID,ucase(c.CenterName) AS CenterName
+                    FROM user_centers u join centers c on u.CenterId = c.ID WHERE u.UserId = ? ORDER BY CenterName';
+            return loadresultset($this->db->dbh,$sql,[$_SESSION['userid']]);
+        }
+    }
+
     public function GetCenterDetails($id)
     {
        $this->db->query('SELECT * FROM centers WHERE ID = :id');
