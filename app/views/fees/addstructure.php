92ec070773e528198ced57165babd1ac4776c191
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-17 23:00:51 +0300

    frontend page for adding fee structure

---
+++ app/views/fees/addstructure.php
@@ -0,0 +1,21 @@
+<?php require APPROOT .'/views/inc/layout/app/header.php'; ?>
+<?php require APPROOT .'/views/inc/layout/app/sidebar.php'; ?>
+<!-- Start Content-->
+<div class="container-fluid">
+    <!-- start page title -->
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right"></div>
+                <h4 class="page-title">
+                    <a href="<?php echo URLROOT;?>/fees/structure" class="btn btn-sm btn-warning ms-2">&larr; Back</a>
+                </h4>
+            </div>
+        </div>
+        <div class="col-12" id="alerBox"></div>
+    </div>     
+    <!-- end page title --> 
+                        
+</div> <!-- container -->
+<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?>                    
+<?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
