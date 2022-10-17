c87d3976f4b0bbb58c2d3dfbcb7e666b1ee9d676
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-17 20:54:46 +0300

    functionality to create and update semisters

--- app/controllers/Semisters.php
+++ app/controllers/Semisters.php
@@ -22,4 +22,72 @@ class Semisters extends Controller
         $this->view('semisters/index',$data);
         exit;
     }
+
+    public function add()
+    {
+        $data = [
+            'title' => 'Add Semister',
+            'id' => 0,
+            'isedit' => false,
+            'semistername' => '',
+            'startdate' => '',
+            'enddate' => '',
+            'has_error' => false,
+            'error' => '',
+        ];
+        $this->view('semisters/add',$data);
+        exit;
+    }
+
+    //create and update
+    public function createupdate()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            //extract json data
+            $fields = json_decode(file_get_contents('php://input'));
+            $data = [
+                'title' => converttobool($fields->isedit) ? 'Edit Semister' : 'Add Semister',
+                'id' => !empty(trim($fields->id)) ? trim($fields->id) : '',
+                'isedit' => converttobool($fields->isedit),
+                'semistername' => !empty(trim($fields->semistername)) ? trim($fields->semistername) : NULL,
+                'startdate' => !empty(trim($fields->startdate)) ? date('Y-m-d',strtotime(trim($fields->startdate))) : NULL,
+                'enddate' => !empty(trim($fields->enddate)) ? date('Y-m-d',strtotime(trim($fields->enddate))) : NULL,
+                'has_error' => false,
+                'error' => '',
+            ];
+            //validate
+            if(is_null($data['semistername']) || is_null($data['startdate']) || is_null($data['enddate'])){
+                http_response_code(400);
+                echo json_encode(['message' => 'Fill all required fields']);
+                exit;
+            }
+            if(!$this->semistermodel->CheckExists($data,'name')){
+                http_response_code(400);
+                echo json_encode(['message' => 'Semister name already exists']);
+                exit;
+            }
+            if($data['startdate'] > $data['enddate']){
+                http_response_code(400);
+                echo json_encode(['message' => 'Start date cannot be greater than end date']);
+                exit;
+            }
+            if(!$this->semistermodel->CheckExists($data,'date')){
+                http_response_code(400);
+                echo json_encode(['message' => 'Defined period conflicts with another period']);
+                exit;
+            }
+            if(!$this->semistermodel->CreateUpdate($data)){
+                http_response_code(500);
+                echo json_encode(['message' => 'Unable to save. Retry or contact admin']);
+                exit;
+            }
+            //saved successfully
+            echo json_encode(['success' => true]);
+            exit;
+
+        }else{
+            redirect('auth/forbidden');
+            exit;
+        }
+    }
 }
--- app/models/Semister.php
+++ app/models/Semister.php
@@ -13,4 +13,52 @@ class Semister
     {
         return loadresultset($this->db->dbh,'SELECT * FROM vw_semisters',[]);
     }
+
+    public function CheckExists($data,$type)
+    {
+        if($type === 'name'){
+            $sql = 'SELECT COUNT(*) FROM semisters WHERE SemisterName = ? AND (ID <> ?) AND (Deleted = 0)';
+            if((int) getdbvalue($this->db->dbh,$sql,[strtolower($data['semistername']),$data['id']]) > 0) {
+                return false;
+            }else{
+                return true;
+            }
+        }elseif($type === 'date'){
+            $sql = 'SELECT COUNT(*) FROM semisters WHERE (? BETWEEN StartDate AND EndDate) AND (Deleted = 0) AND (ID <> ?)';
+            if((int) getdbvalue($this->db->dbh,$sql,[strtolower($data['startdate']),$data['id']]) > 0) {
+                return false;
+            }else{
+                return true;
+            }
+        }
+    }
+
+    public function CreateUpdate($data)
+    {
+        try {
+            if($data['isedit']){
+                $this->db->query('UPDATE semisters SET SemisterName=:sname,StartDate=:sdate,EndDate=:edate 
+                                  WHERE (ID = :id)');
+            }else{
+                $this->db->query('INSERT INTO semisters (SemisterName,StartDate,EndDate) VALUES(:sname,:sdate,:edate)');
+            }
+            $this->db->bind(':sname',strtolower($data['semistername']));
+            $this->db->bind(':sdate',$data['startdate']);
+            $this->db->bind(':edate',$data['enddate']);
+            if($data['isedit']){
+                $this->db->bind(':id',$data['id']);
+            }
+            if(!$this->db->execute()){
+                return false;
+            }else{
+                return true;
+            }
+        } catch (PDOException $e) {
+            error_log($e->getMessage(),0);
+            return false;
+        } catch (Exception $e) {
+            error_log($e->getMessage(),0);
+            return false;
+        }
+    }
 }
