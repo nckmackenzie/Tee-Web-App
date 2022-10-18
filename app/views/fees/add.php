11ee4e395f02806607d50a9fdafedd0a5573fdcc
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-18 20:00:20 +0300

    add save class to save button

--- app/views/fees/add.php
+++ app/views/fees/add.php
@@ -130,7 +130,7 @@
                         <div class="d-grid d-md-block">
                             <input type="hidden" name="id" value="<?php echo $data['id'];?>">
                             <input type="hidden" name="isedit" value="<?php echo $data['isedit'];?>">
-                            <button type="submit" class="btn btn-sm btn-primary">Save</button>
+                            <button type="submit" class="btn btn-sm btn-primary save">Save</button>
                         </div>
                     </form>
                 </div>
