96e23479f225a5a97924d5517106f4edd19334ae
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-18 10:09:54 +0300

    frontend for creating and updating fee structure

--- app/views/fees/addstructure.php
+++ app/views/fees/addstructure.php
@@ -15,7 +15,42 @@
         <div class="col-12" id="alerBox"></div>
     </div>     
     <!-- end page title --> 
-                        
+    <div class="row">
+        <div class="col-md-9 mx-auto">
+            <div class="card">
+                <div class="card-header"><?php echo $data['title'];?></div>
+                <div class="card-body">
+                    <form action="" method="post" id="structureForm" autocomplete="off">
+                        <div class="row">
+                            <div class="col-md-12 mb-3">
+                                <label for="semister">Semister</label>
+                                <select name="semister" id="semister" class="form-select form-select-sm mandatory">
+                                    <option value="" selected disabled>Select semister</option>
+                                    <?php foreach($data['semisters'] as $semister) : ?>
+                                        <option value="<?php echo $semister->ID;?>"><?php echo $semister->SemisterName;?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                                <span class="invalid-feedback"></span>
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label for="amount">Total Fee Required</label>
+                                <input type="number" name="amount" id="amount" 
+                                       class="form-control form-control-sm mandatory"
+                                       value="<?php echo $data['amount'];?>">
+                                <span class="invalid-feedback"></span>
+                            </div>
+                        </div>
+                        <div class="d-grid d-md-block">
+                            <input type="hidden" name="id" value="<?php echo $data['id'];?>">
+                            <input type="hidden" name="isedit" value="<?php echo $data['isedit'];?>">
+                            <button type="submit" class="btn btn-sm btn-primary save">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>                    
 </div> <!-- container -->
-<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?>                    
+<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?>
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/fees/add-structure.js"></script>                    
 <?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
