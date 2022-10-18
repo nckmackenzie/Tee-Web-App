6323c30b0965dafd08e28a75f940d30d6884dd13
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-18 19:54:11 +0300

    add functions to format numbers

--- public/dist/js/utils/utils.js
+++ public/dist/js/utils/utils.js
@@ -3,3 +3,18 @@ export const loadingButton = (btn, text) => {
   btn.disabled = true;
   btn.innerHTML = html;
 };
+
+export function numberFormatter(number) {
+  if (number.includes(',')) {
+    return number.replaceAll(',', '');
+  }
+  return number;
+}
+
+export function getSelectedText(sel) {
+  return sel.options[sel.selectedIndex].text;
+}
+
+export function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
