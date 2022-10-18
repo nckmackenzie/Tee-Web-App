76ed2e7c980468e324f1c5e5625381042dcf7c5b
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-18 19:58:42 +0300

    remove ssr validation classes and span text contents

--- app/views/fees/add.php
+++ app/views/fees/add.php
@@ -28,32 +28,30 @@
                             <div class="col-md-4 mb-3">
                                 <label for="pdate">Payment Date</label>
                                 <input type="date" name="pdate" id="pdate" 
-                                       class="form-control form-control-sm mandatory 
-                                       <?php echo inputvalidation($data['pdate'],$data['pdate_err'],$data['touched']);?>"
+                                       class="form-control form-control-sm mandatory"
                                        value="<?php echo $data['pdate'];?>">
-                                <span class="invalid-feedback"><?php echo $data['pdate_err'];?></span>
+                                <span class="invalid-feedback"></span>
                             </div>
                             <div class="col-md-5 mb-3">
                                 <label for="student">Student</label>
                                 <select name="student" id="student" 
-                                        class="form-select form-select-sm mandatory 
-                                        <?php echo inputvalidation($data['student'],$data['student_err'],$data['touched']);?>">
+                                        class="form-select form-select-sm mandatory">
                                     <option value="" selected disabled>Select student</option>
                                     <?php foreach($data['students'] as $student) : ?>
                                         <option value="<?php echo $student->ID;?>" <?php selectdCheck($data['student'],$student->ID);?>><?php echo $student->StudentName;?></option>
                                     <?php endforeach; ?>
                                 </select>
-                                <span class="invalid-feedback"><?php echo $data['semister_err'];?></span>
+                                <span class="invalid-feedback"></span>
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="semister">Semister</label>
-                                <select name="semister" id="semister" class="form-select form-select-sm 
-                                <?php echo inputvalidation($data['semister'],$data['semister_err'],$data['touched']);?>">
+                                <select name="semister" id="semister" class="form-select form-select-sm mandatory">
                                     <option value="">Select semister</option>
                                     <?php foreach($data['semisters'] as $semister) : ?>
                                         <option value="<?php echo $semister->ID;?>" <?php selectdCheck($data['semister'],$semister->ID);?>><?php echo $semister->SemisterName;?></option>
                                     <?php endforeach; ?>   
                                 </select>
+                                <span class="invalid-feedback"></span>
                             </div>
                             <div class="col-md-3 mb-3">
                                 <label for="balancebf">Balance B/f</label>            
@@ -63,7 +61,7 @@
                                        readonly>
                             </div>
                             <div class="col-md-3 mb-3">
-                                <label for="totalfee">Semister Fees</label>            
+                                <label for="semisterfees">Semister Fees</label>            
                                 <input type="text" name="semisterfees" id="semisterfees" 
                                        class="form-control form-control-sm" 
                                        value="<?php echo $data['semisterfees'];?>"
@@ -86,44 +84,40 @@
                             <div class="col-md-4 mb-3">
                                 <label for="amount">Amount Paid</label>
                                 <input type="number" name="amount" id="amount" 
-                                       class="form-control form-control-sm mandatory 
-                                       <?php echo inputvalidation($data['amount'],$data['amount_err'],$data['touched']);?>"
+                                       class="form-control form-control-sm mandatory"
                                        value="<?php echo $data['amount'];?>"
                                        placeholder="eg 6,000">
-                                <span class="invalid-feedback"><?php echo $data['amount_err'];?></span>
+                                <span class="invalid-feedback"></span>
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="account">G/L Account</label>
                                 <select name="account" id="account" 
-                                        class="form-select form-select-sm mandatory 
-                                        <?php echo inputvalidation($data['account'],$data['account_err'],$data['account']);?>">
+                                        class="form-select form-select-sm mandatory">
                                     <option value="" selected disabled>Select account</option>
                                     <?php foreach($data['accounts'] as $account) : ?>
                                         <option value="<?php echo $account->ID;?>" <?php selectdCheck($data['account'],$account->ID);?>><?php echo $account->AccountName;?></option>
                                     <?php endforeach; ?>
                                 </select>
-                                <span class="invalid-feedback"><?php echo $data['account_err'];?></span>
+                                <span class="invalid-feedback"></span>
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="paymethod">Payment Method</label>
                                 <select name="paymethod" id="paymethod" 
-                                        class="form-select form-select-sm mandatory 
-                                        <?php echo inputvalidation($data['paymethod'],$data['paymethod_err'],$data['paymethod']);?>">
+                                        class="form-select form-select-sm mandatory">
                                     <option value="" selected disabled>Select pay method</option>
                                     <option value="1" <?php selectdCheck($data['paymethod'],1);?>>Cash</option>
                                     <option value="2" <?php selectdCheck($data['paymethod'],2);?>>Cheque</option>
                                     <option value="3" <?php selectdCheck($data['paymethod'],3);?>>Bank</option>
                                 </select>
-                                <span class="invalid-feedback"><?php echo $data['paymethod_err'];?></span>
+                                <span class="invalid-feedback"></span>
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="reference">Payment Reference</label>
                                 <input type="text" name="reference" id="reference" 
-                                       class="form-control form-control-sm mandatory 
-                                       <?php echo inputvalidation($data['reference'],$data['reference_err'],$data['touched']);?>"
+                                       class="form-control form-control-sm mandatory"
                                        value="<?php echo $data['reference'];?>"
                                        placeholder="eg XM45221255X">
-                                <span class="invalid-feedback"><?php echo $data['reference_err'];?></span>
+                                <span class="invalid-feedback"></span>
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="narration">Narration</label>
@@ -144,5 +138,6 @@
         </div>
     </div>                    
 </div> <!-- container -->
-<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?>                    
+<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?>  
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/fees/add-fees.js"></script>                  
 <?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
