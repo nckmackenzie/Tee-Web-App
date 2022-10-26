23bab1a0c80d9395e760a8a55b5452510ffbe46c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-26 20:39:51 +0300

    save functionality

--- app/controllers/Banks.php
+++ app/controllers/Banks.php
@@ -36,4 +36,50 @@ class Banks extends Controller
         $this->view('banks/add',$data);
         exit;
     }
+
+    public function createupdate()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            $fields = json_decode(file_get_contents('php://input'));
+            $data = [
+                'id' => isset($fields->id) && !empty(trim($fields->id)) ? (int)trim($fields->id) : null,
+                'isedit' => converttobool($fields->isedit),
+                'bankname' => isset($fields->bankname) && !empty(trim($fields->bankname)) ? strtolower(trim($fields->bankname)) : null,
+                'accountno' => isset($fields->accountno) && !empty(trim($fields->accountno)) ? trim($fields->accountno) : null,
+                'openingbal' => converttobool($fields->isedit) ? null : (isset($fields->openingbal) && !empty(trim($fields->openingbal)) ? floatval(trim($fields->openingbal)) : null),
+                'asof' => converttobool($fields->isedit) ? null : (isset($fields->asof) && !empty(trim($fields->asof)) ? date('Y-m-d',strtotime(trim($fields->asof))) : null),
+            ];
+
+            //validate
+            if(is_null($data['bankname'])){
+                http_response_code(400);
+                echo json_encode(['message' => 'Enter bank name']);
+                exit;
+            }
+            if(!$data['isedit'] && $data['openingbal'] > 0 && is_null($data['asof'])){
+                http_response_code(400);
+                echo json_encode(['message' => 'Select date of balance']);
+                exit;
+            }
+            if(!$data['isedit'] && $data['asof'] > date('Y-m-d')){
+                http_response_code(400);
+                echo json_encode(['message' => 'Invalid date of balance']);
+                exit;
+            }
+
+            if(!$this->bankmodel->CreateUpdate($data)){
+                http_response_code(500);
+                echo json_encode(['message' => 'Unable to save bank. Retry or contact admin']);
+                exit;
+            }
+
+            echo json_encode(['success' => true]);
+            exit;
+        }
+        else{
+            redirect('auth/forbidden');
+            exit;
+        }
+    }
 }
--- app/models/Bank.php
+++ app/models/Bank.php
@@ -14,4 +14,49 @@ class Bank
         $sql = 'SELECT ID,UCASE(BankName) AS BankName,AccountNo FROM banks WHERE Deleted = 0 ORDER BY BankName';
         return loadresultset($this->db->dbh,$sql,[]);
     }
+
+    function Save($data)
+    {
+        try {
+
+            $this->db->dbh->beginTransaction();
+
+            $this->db->query('INSERT INTO banks (BankName,AccountNo) VALUES(:bname,:accno)');
+            $this->db->bind(':bname',$data['bankname']);
+            $this->db->bind(':accno',$data['accountno']);
+            $this->db->execute();
+            $tid= $this->db->dbh->lastInsertId();
+
+            if($data['openingbal'] > 0){
+                $narr = $data['bankname'] . ' opening balance';
+                savetoledger($this->db->dbh,$data['asof'],'cash at bank',$data['openingbal'],0,
+                             $narr,3,8,$tid,$_SESSION['centerid']);
+
+                savetoledger($this->db->dbh,$data['asof'],'opening balance equity',0,$data['openingbal'],
+                             $narr,6,8,$tid,$_SESSION['centerid']);
+
+                savebankposting($this->db->dbh,$data['asof'],$tid,$data['openingbal'],0,null,$narr,8,$tid,$_SESSION['centerid']);
+            }
+
+            if(!$this->db->dbh->commit()){
+                return false;
+            }else{
+                return true;
+            }
+            
+        } catch (PDOException $e) {
+            if($this->db->dbh->inTransaction()){
+                $this->db->dbh->rollback();
+            }
+            error_log($e->getMessage(),0);
+            return false;
+        }
+    }
+
+    public function CreateUpdate($data)
+    {
+        if(!$data['isedit']){
+            return $this->save($data);
+        }
+    }
 }
