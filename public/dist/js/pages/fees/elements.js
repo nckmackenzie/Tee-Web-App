3fccc4665eb8643ac381be883a282513f5a20b7f
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-27 15:58:14 +0300

    declare basic elements

---
+++ public/dist/js/pages/fees/elements.js
@@ -0,0 +1,7 @@
+export const receiptnoInput = document.getElementById('receiptno');
+export const currentInput = document.getElementById('currentreceiptno');
+export const btnleft = document.querySelector('.btnleft');
+export const btnright = document.querySelector('.btnright');
+export const form = document.getElementById('graduation-fees');
+export const paydateInput = document.getElementById('paydate');
+export const savebtn = document.querySelector('.btnsave');
