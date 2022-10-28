8b95c3d479936badde45f7f37cfad9a2e4259273
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-28 22:34:33 +0300

    functionality to save payments

--- app/controllers/Payments.php
+++ app/controllers/Payments.php
@@ -27,8 +27,94 @@ class Payments extends Controller
     {
         $data = [
             'title' => 'New payments',
+            'payid' => $this->paymentmodel->GetPayId(),
+            'invoices' => $this->paymentmodel->GetInvoicesWithBalances(),
+            'totaldue' => 0
         ];
+
+        if(!empty($data['invoices'])){
+            $total = 0;
+            foreach($data['invoices'] as $invoice){
+                $total += floatval($invoice->OpeningBal);
+            }
+            $data['totaldue'] = $total;
+        }
+
         $this->view('payments/add',$data);
         exit;
+    }
+
+    public function create()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            $fields = json_decode(file_get_contents('php://input')); //extract json data
+            if(empty($fields->header)){
+                http_response_code(400);
+                echo json_encode(['message' => 'Missing header data']);
+                exit;
+            }
+            if(empty($fields->details)){
+                http_response_code(400);
+                echo json_encode(['message' => 'No payments made']);
+                exit;
+            }
+            $header = $fields->header;
+            $details = $fields->details;
+            $data = [
+                'paydate' => isset($header->paydate) && !empty(trim($header->paydate)) ? date('Y-m-d',strtotime(trim($header->paydate))) : null,
+                'paymethod' => isset($header->paymethod) && !empty(trim($header->paymethod)) ? (int)trim($header->paymethod) : null,
+                'details' => $details,
+                'cheque_err' => '',
+                'overpay_err' => ''
+            ];
+            $invalidreferenceerrors = 0;
+            $overpayerrors = 0;
+            $emptypayments = 0;
+            foreach($details as $detail){
+                if(empty(trim($detail->payment))){
+                    $emptypayments ++;
+                }
+            }
+            if((int)$emptypayments > 0){
+                http_response_code(400);
+                echo json_encode(['message' => $emptypayments . ' missing payment amounts']);
+                exit;
+            }
+            foreach($details as $detail){
+                if(empty(trim($detail->cheque))){
+                    $invalidreferenceerrors ++;
+                }
+            }
+            if((int)$invalidreferenceerrors > 0){
+                http_response_code(400);
+                echo json_encode(['message' => $invalidreferenceerrors . ' missing payment reference']);
+                exit;
+            }
+            foreach($details as $detail){
+                if(floatval(trim($detail->balance)) < floatval(trim($detail->payment))){
+                    $overpayerrors ++;
+                }
+            }
+            if((int)$overpayerrors > 0){
+                http_response_code(400);
+                echo json_encode(['message' => $overpayerrors . ' overpayments on invoice']);
+                exit;
+            }
+
+            if(!$this->paymentmodel->Create($data)){
+                http_response_code(500);
+                echo json_encode(['message' => 'Unable to save payments. Retry or contact admin']);
+                exit;
+            }
+
+            echo json_encode(['success' => true]);
+            exit;
+
+        }else
+        {
+            redirect('auth/forbidden');
+            exit;
+        }
     }
 }
--- app/models/Payment.php
+++ app/models/Payment.php
@@ -8,4 +8,76 @@ class Payment
     {
         $this->db = new Database;
     }
+
+    public function GetPayId()
+    {
+        return getuniqueid($this->db->dbh,'PayId','invoice_payments',$_SESSION['centerid'],false);
+    }
+
+    public function GetInvoicesWithBalances()
+    {
+        return loadresultset($this->db->dbh,'SELECT * FROM vw_invoice_with_balances',[]);
+    }
+
+    public function Create($data)
+    {
+        try {
+            $this->db->dbh->beginTransaction();
+            $payid = $this->GetPayId();
+
+        for ($i=0; $i < count($data['details']); $i++) { 
+            $narr = 'invoice payment reference' .$data['details'][$i]->cheque;
+            $this->db->query('INSERT INTO invoice_payments(TransactionDate,HeaderId,SupplierId,PayId,Debit,Narration,PaymentMethod,
+                                                           PaymentReference,TransactionType,CenterId) 
+                              VALUES(:tdate,:hid,:supplier,:payid,:debit,:narr,:paymethod,:ref,:ttype,:cid)');
+            $this->db->bind(':tdate',$data['paydate']);
+            $this->db->bind(':hid',$data['details'][$i]->invoiceid);
+            $this->db->bind(':supplier',$data['details'][$i]->sid);
+            $this->db->bind(':payid',$payid);
+            $this->db->bind(':debit',floatval($data['details'][$i]->payment));
+            $this->db->bind(':narr',$narr);
+            $this->db->bind(':paymethod',$data['paymethod']);
+            $this->db->bind(':ref',strtolower($data['details'][$i]->cheque));
+            $this->db->bind(':ttype',4);
+            $this->db->bind(':cid',$_SESSION['centerid']);
+            $this->db->execute();
+            $tid = $this->db->dbh->lastInsertId();
+
+            $status = 2;
+            if(floatval($data['details'][$i]->payment) == floatval($data['details'][$i]->balance)){
+                $status = 1;
+            }
+            //update header table
+            $this->db->query('UPDATE invoice_header SET PayStatus = :pstatus WHERE (ID = :id)');
+            $this->db->bind(':pstatus',$status);
+            $this->db->bind(':id',$data['details'][$i]->invoiceid);
+            $this->db->execute();
+
+            savetoledger($this->db->dbh,$data['paydate'],'accounts payable',$data['details'][$i]->payment,0
+                         ,$narr,4,4,$tid,$_SESSION['centerid']);
+            if((int)$data['paymethod'] === 1){
+                savetoledger($this->db->dbh,$data['paydate'],'cash at hand',0,$data['details'][$i]->payment,
+                             $narr,3,4,$tid,$_SESSION['centerid']);
+            }else{
+                savetoledger($this->db->dbh,$data['paydate'],'cash at bank',0,$data['details'][$i]->payment,
+                             $narr,3,4,$tid,$_SESSION['centerid']);
+                savebankposting($this->db->dbh,$data['paydate'],(int)$data['paymethod'] === 2 ? 1 : 0,null,0,
+                                floatval($data['details'][$i]->payment),$data['details'][$i]->cheque,$narr,4,$tid,$_SESSION['centerid']);
+            }
+        }
+
+        if(!$this->db->dbh->commit()){
+            return false;
+        }else{
+            return true;
+        }
+            
+        } catch (PDOException $e) {
+            if($this->db->dbh->inTransaction()){
+                $this->db->dbh->rollBack();
+            }
+            error_log($e->getMessage(),0);
+            return false;
+        }
+    }
 }
