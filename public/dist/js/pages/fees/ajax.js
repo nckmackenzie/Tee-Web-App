24f2659dc7be6be78d8dbfc5146099919026ccbf
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-28 13:52:15 +0300

    add form submit ajax

--- public/dist/js/pages/fees/ajax.js
+++ public/dist/js/pages/fees/ajax.js
@@ -4,9 +4,28 @@ export async function getReceiptNo() {
   return await getRequest(`${HOST_URL}/fees/getgraduationpaymentid`);
 }
 
+export async function getFirstAndLastIds() {
+  return await getRequest(`${HOST_URL}/fees/getfirstandlastids`);
+}
+//garduation fee submission
+export async function saveGraduationPayment(formdata) {
+  const response = await sendHttpRequest(
+    `${HOST_URL}/fees/graudationcreateedit`,
+    'POST',
+    JSON.stringify(formdata),
+    { 'Content-Type': 'application/json' },
+    alerBox
+  );
+
+  return response;
+}
+
 export async function getTransaction(receiptno) {
   const response = await getRequest(
     `${HOST_URL}/fees/getgraduationtxn?receiptno=${receiptno}`
   );
+  // if (response.success) {
+  //   return response.results;
+  // }
   return response;
 }
