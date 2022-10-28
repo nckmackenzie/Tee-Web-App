d2fda0f20c3c6e9011ca7ea76a1e429c50dbbd8c
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-28 13:34:14 +0300

    get transaction

--- public/dist/js/pages/fees/ajax.js
+++ public/dist/js/pages/fees/ajax.js
@@ -4,18 +4,9 @@ export async function getReceiptNo() {
   return await getRequest(`${HOST_URL}/fees/getgraduationpaymentid`);
 }
 
-export async function getFirstAndLastIds() {
-  return await getRequest(`${HOST_URL}/fees/getfirstandlastids`);
-}
-//garduation fee submission
-export async function saveGraduationPayment(formdata) {
-  const response = await sendHttpRequest(
-    `${HOST_URL}/fees/graudationcreateedit`,
-    'POST',
-    JSON.stringify(formdata),
-    { 'Content-Type': 'application/json' },
-    alerBox
+export async function getTransaction(receiptno) {
+  const response = await getRequest(
+    `${HOST_URL}/fees/getgraduationtxn?receiptno=${receiptno}`
   );
-
   return response;
 }
