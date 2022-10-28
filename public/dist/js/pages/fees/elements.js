f5c77f6ba2242a20880e198c02df9e5a2107c243
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-28 13:52:36 +0300

    elemets definitions

--- public/dist/js/pages/fees/elements.js
+++ public/dist/js/pages/fees/elements.js
@@ -1,7 +1,15 @@
 export const receiptnoInput = document.getElementById('receiptno');
+export const studentSelect = document.getElementById('student');
+export const groupSelect = document.getElementById('group');
+export const accountSelect = document.getElementById('account');
+export const paymethodSelect = document.getElementById('paymethod');
+export const amountInput = document.getElementById('paid');
+export const referenceInput = document.getElementById('reference');
 export const currentInput = document.getElementById('currentreceiptno');
-export const btnleft = document.querySelector('.btnleft');
-export const btnright = document.querySelector('.btnright');
 export const form = document.getElementById('graduation-fees');
+export const searchForm = document.getElementById('search-form');
 export const paydateInput = document.getElementById('paydate');
 export const savebtn = document.querySelector('.btnsave');
+export const spinnerContainer = document.querySelector('.spinner-container');
+export const idInput = document.querySelector('#id');
+export const iseditInput = document.querySelector('#isedit');
