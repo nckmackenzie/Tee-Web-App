43f437b1b67dc8876d8848e5ad9fdfa843d35083
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-30 11:12:24 +0300

    page not found default for bankings

---
+++ app/views/bankings/index.php
@@ -0,0 +1,25 @@
+<?php require APPROOT .'/views/inc/layout/app/header.php'; ?>
+<?php require APPROOT .'/views/inc/layout/app/sidebar.php'; ?>
+<!-- Start Content-->
+<div class="container-fluid">
+    <!-- start page title -->
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-lg-6 mx-auto">
+                <div class="alert alert-danger" role="alert">
+                    <h2 class="h2">404! Page Not Found</h2>
+                    <p>You are trying to access a page that is not available!</p>
+                </div>
+            </div>
+        </div>
+    </div>     
+    <!-- end page title --> 
+                        
+</div> <!-- container -->
+<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?>                    
+<?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
