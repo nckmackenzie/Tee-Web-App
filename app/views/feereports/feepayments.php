71da14f7b222cb3ea02f141cee4bb50836e2c8e6
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-11-01 19:32:16 +0300

    front end for fees payments report

---
+++ app/views/feereports/feepayments.php
@@ -0,0 +1,41 @@
+<?php require APPROOT .'/views/inc/layout/app/header.php'; ?>
+<?php require APPROOT .'/views/inc/layout/app/sidebar.php'; ?>
+<!-- Start Content-->
+<div class="container-fluid">
+    <!-- start page title -->
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right"></div>
+                <h4 class="page-title"></h4>
+            </div>
+        </div>
+    </div>     
+    <!-- end page title --> 
+    <div class="row mb-1">
+        <div class="col-md-3 sm-m-3">
+            <label for="start">Start Date</label>
+            <input type="date" name="start" id="start" class="form-control form-control-sm">
+            <span class="invalid-feedback startspan"></span>
+        </div>
+        <div class="col-md-3">
+            <label for="end">End Date</label>
+            <input type="date" name="end" id="end" class="form-control form-control-sm">
+            <span class="invalid-feedback endspan"></span>
+        </div>
+    </div> 
+    <div class="row">
+        <div class="col-md-2">
+            <button class="btn btn-sm btn-primary preview">Preview</button>
+        </div>
+        <div class="col-12 mt-2">
+            <div class="table-responsive">
+                <div id="results"></div>
+            </div>
+        </div>
+    </div> 
+                       
+</div> <!-- container -->
+<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?> 
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/reports/fees/feePayments.js"></script>                   
+<?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
