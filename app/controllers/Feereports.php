09654a674fcd112924e45bda19f43e9b536eaa12
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-11-01 19:33:00 +0300

    methods for handling fee payments reports

--- app/controllers/Feereports.php
+++ app/controllers/Feereports.php
@@ -17,4 +17,52 @@ class Feereports extends Controller
         $this->view('auth/notfound',$data);
         exit;
     }
+
+    public function feepayments()
+    {
+        checkrights($this->authmodel,'fee payments');
+        $data = [
+            'title' => 'Fee payment report',
+            'has_datatable' => true
+        ];
+        $this->view('feereports/feepayments',$data);
+        exit;
+    }
+
+    public function feepaymentsrpt()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+            $_GET = filter_input_array(INPUT_GET,FILTER_UNSAFE_RAW);
+            $data = [
+                'sdate' => isset($_GET['sdate']) && !empty(trim($_GET['sdate'])) ? date('Y-m-d',strtotime($_GET['sdate'])) : null,
+                'edate' => isset($_GET['edate']) && !empty(trim($_GET['edate'])) ? date('Y-m-d',strtotime($_GET['edate'])) : null,
+                'results' => []
+            ];
+
+            if(is_null($data['sdate']) || is_null($data['edate'])){
+                http_response_code(400);
+                echo json_encode(['message' => 'Provide all required fields']);
+                exit;
+            }
+            $results = $this->reportmodel->Getfeepayments($data);
+            if(empty($results )){
+                http_response_code(404);
+                echo json_encode(['message' => 'No data found']);
+                exit;
+            }
+            foreach ($results as $result):
+                array_push($data['results'],[
+                    'paymentDate' => $result->PaymentDate,
+                    'receiptNo' => $result->ReceiptNo,
+                    'studentName' => $result->StudentName,
+                    'amount' => $result->AmountPaid,
+                    'paymentReference' => $result->PaymentReference
+                ]);
+            endforeach;
+            echo json_encode($data['results']);
+        }else{
+            redirect('auth/forbidden');
+            exit();
+        }
+    }
 }
