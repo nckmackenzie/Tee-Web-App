8f5fb92b1312f2eeb80b63d2a8f558a4f7f014d3
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-11-01 19:34:58 +0300

    add GetFeepayments method

--- app/models/Feereport.php
+++ app/models/Feereport.php
@@ -7,4 +7,12 @@ class Feereport
     {
         $this->db = new Database;
     }
+
+    public function Getfeepayments($data)
+    {
+        $this->db->query('CALL sp_feepayment_by_date(:sdate,:edate)');
+        $this->db->bind(':sdate',date('Y-m-d',strtotime($data['sdate'])));
+        $this->db->bind(':edate',date('Y-m-d',strtotime($data['edate'])));
+        return $this->db->resultset();
+    }
 }
