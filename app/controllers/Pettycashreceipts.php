383cc45383478de9f2a98968c49ddc732fb38dbc
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-11-03 14:08:00 +0300

    functionality to save new petty cash receipts

--- app/controllers/Pettycashreceipts.php
+++ app/controllers/Pettycashreceipts.php
@@ -22,4 +22,71 @@ class Pettycashreceipts extends Controller
         $this->view('pettycashreceipts/index', $data);
         exit;
     }
+
+    public function add()
+    {
+        $data = [
+            'title' => 'Add Petty Cash Receipt',
+            'receiptno' => $this->receiptmodel->GetReceiptNo(),
+            'id' => 0,
+            'isedit' => false
+        ];
+        $this->view('pettycashreceipts/add', $data);
+        exit;
+    }
+
+    public function CreateUpdate()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            $fields = json_decode(file_get_contents('php://input'));
+            if(empty($fields)){
+                http_response_code(400);
+                echo json_encode(['message' => 'Unable to extract submitted data']);
+                exit;
+            }
+
+            $data = [
+                'id' => isset($fields->id) && !empty(trim($fields->id)) ? (int)trim($fields->id) : 0,
+                'isedit' => isset($fields->id) ? converttobool($fields->isedit) : false,
+                'receiptdate' => isset($fields->receiptdate) && !empty(trim($fields->receiptdate)) ? date('Y-m-d',strtotime(trim($fields->receiptdate))) : null,
+                'amount' => isset($fields->amount) && !empty(trim($fields->amount)) ? floatval(trim($fields->amount)) : null,
+                'reference' => isset($fields->reference) && !empty(trim($fields->reference)) ? strtolower(trim($fields->reference)) : null,
+                'narration' => isset($fields->narration) && !empty(trim($fields->narration)) ? strtolower(trim($fields->narration)) : null,
+            ];
+
+            //validate
+            if(is_null($data['receiptdate']) || is_null($data['amount']) || is_null($data['reference'])){
+                http_response_code(400);
+                echo json_encode(['message' => 'Provide all required fields']);
+                exit;
+            }
+            if(!$this->receiptmodel->CheckReferenceExists($data['reference'],$data['id'])){
+                http_response_code(400);
+                echo json_encode(['message' => 'Reference / Cheque no already exists']);
+                exit;
+            }
+
+            if(!$this->receiptmodel->CreateUpdate($data)){
+                http_response_code(500);
+                echo json_encode(['message' => 'Something went wrong while saving. Contact system admin']);
+                exit;
+            }
+
+            echo json_encode(['success' => true]);
+            exit;
+
+        }
+        else
+        {
+            redirect('auth/forbidden');
+            exit;
+        }
+    }
+
+    public function getnewid()
+    {
+        $newid = $this->receiptmodel->GetReceiptNo();
+        echo json_encode(['success' => true, 'newid' => $newid]);
+    }
 }
--- app/models/Pettycashreceipt.php
+++ app/models/Pettycashreceipt.php
@@ -12,4 +12,88 @@ class Pettycashreceipt
     {
         return loadresultset($this->db->dbh,'SELECT * FROM vw_petty_cash',[]);
     }
+
+    public function GetReceiptNo()
+    {
+        return getuniqueid($this->db->dbh,'ReceiptNo','pettycash',$_SESSION['centerid'],false);
+    }
+
+    public function CheckReferenceExists($ref,$id)
+    {
+        $count =  getdbvalue($this->db->dbh,'SELECT COUNT(*) 
+                                             FROM pettycash 
+                                             WHERE (Deleted = 0) AND (Reference = ?) AND (ID <> ?)',[$ref,$id]);
+        if((int)$count > 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public function Save($data)
+    {
+        try {
+            $this->db->dbh->beginTransaction();
+            $receipt = $this->GetReceiptNo();
+
+            if(!$data['isedit']){
+                $this->db->query('INSERT INTO pettycash (ReceiptNo,TransactionDate,Debit,Reference,Narration,TransactionType,CenterId)
+                                  VALUES(:receipt,:tdate,:debit,:ref,:narr,:ttype,:cid)');
+                $this->db->bind(':receipt',$receipt);
+            }else{
+                $this->db->query('UPDATE pettycash SET TransactionDate=:tdate,Debit=:debit,Reference=:ref,Narration=:narr
+                                  WHERE (ID = :id)');
+            }
+            $this->db->bind(':tdate',$data['receiptdate']);
+            $this->db->bind(':debit',$data['amount']);
+            $this->db->bind(':ref',$data['reference']);
+            $this->db->bind(':narr',$data['narration']);
+            if(!$data['isedit']){
+                $this->db->bind(':ttype',10);
+                $this->db->bind(':cid',$_SESSION['centerid']);
+            }else{
+                $this->db->bind(':id',$data['id']);
+            }
+            $this->db->execute();
+            $tid = $data['isedit'] ? $data['id'] : $this->db->dbh->lastInsertId();
+
+            if($data['isedit']){
+                $this->db->query('DELETE FROM ledger WHERE (TransactionType = 10) AND (TransactionId = :id)');
+                $this->db->bind(':id',$data['id']);
+                $this->db->execute();
+
+                $this->db->query('DELETE FROM bankpostings WHERE (TransactionType = 10) AND (TransactionId = :id)');
+                $this->db->bind(':id',$data['id']);
+                $this->db->execute();
+            }
+
+            savetoledger($this->db->dbh,$data['receiptdate'],'cash at bank',0,$data['amount'],
+                         $data['narration'],3,10,$tid,$_SESSION['centerid']);
+            savetoledger($this->db->dbh,$data['receiptdate'],'petty cash',$data['amount'],0,
+                         $data['narration'],3,10,$tid,$_SESSION['centerid']);
+            savebankposting($this->db->dbh,$data['receiptdate'],0,null,0,$data['amount'],
+                            $data['reference'],$data['narration'],10,$tid,$_SESSION['centerid']);             
+            
+            if(!$this->db->dbh->commit()){
+                return false;
+            }else{
+                return true;
+            }
+            
+        } catch (PDOException $e) {
+            if($this->db->dbh->inTransaction()){
+                $this->db->dbh->rollback();
+            }
+            error_log($e->getMessage(),0);
+        }
+    }
+
+    public function CreateUpdate($data)
+    {
+       if(!$data['isedit']){
+            return $this->Save($data);
+       }else{
+
+       }
+    }
 }
