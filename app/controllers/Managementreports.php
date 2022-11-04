e539a6a61d74bc7ea4dd52e565a28937807cad4f
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-11-04 19:43:03 +0300

    methods for trial balance functionalities

--- app/controllers/Managementreports.php
+++ app/controllers/Managementreports.php
@@ -63,4 +63,73 @@ class Managementreports extends Controller
             exit;
         }
     }
+
+    public function trialbalance()
+    {
+        $data = [
+            'title' => 'Trial Balance',
+            'has_datatable' => true
+        ];
+        $this->view('managementreports/trialbalance',$data);
+        exit;
+    }
+
+    public function trialbalancerpt()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'GET')
+        {
+            $_GET = filter_input_array(INPUT_GET,FILTER_UNSAFE_RAW);
+            $data = [
+                'sdate' => isset($_GET['sdate']) && !empty(trim($_GET['sdate'])) ? date('Y-m-d',strtotime(trim($_GET['sdate']))) : null,
+                'edate' => isset($_GET['edate']) && !empty(trim($_GET['edate'])) ? date('Y-m-d',strtotime(trim($_GET['edate']))) : null,
+            ];
+            if(is_null($data['sdate']) || is_null($data['edate'])){
+                http_response_code(400);
+                echo json_encode(['message' => 'Provide all required fields']);
+                exit;
+            }
+            if($data['sdate'] > $data['edate']){
+                http_response_code(400);
+                echo json_encode(['message' => 'Start date cannot be greater than end date']);
+                exit;
+            }
+            //get tb result
+            $accounts = $this->reportmodel->GetTrialBalanceReport($data);
+            $results = [];
+            $debitstotal = 0;
+            $creditstotal = 0;
+
+            if(empty($accounts)){
+                array_push($results,[
+                    'account' => '',
+                    'debit' => '',
+                    'credits' => '',
+                ]);
+            }else{
+                foreach($accounts as $account)
+                {
+                    $debitstotal += floatval($account->Debit);
+                    $creditstotal += floatval($account->credit);
+                    if(floatval($account->Debit) !==0 && floatval($account->credit) !==0):
+                        array_push($results,[
+                            'account' => ucwords($account->Account),
+                            'debit' => floatval($account->Debit) == 0 ? '' : floatval($account->Debit), 
+                            'credit' => floatval($account->credit) == 0 ? '' : floatval($account->credit)
+                        ]);
+                    endif;
+                }
+            }
+
+            echo json_encode(['success' => true,
+                              'results' => $results,
+                              'debitstotal' => $debitstotal,
+                              'creditstotal' => $creditstotal]);
+            exit;
+        }
+        else
+        {
+            redirect('auth/forbidden');
+            exit;
+        }
+    }
 }
