83ff453f93f8533dd18502880ae3cf39a44aa757
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-12-09 16:11:44 +0300

    refactor of sales report

--- app/views/reports/salesreport.php
+++ app/views/reports/salesreport.php
@@ -4,6 +4,7 @@
 <div class="container-fluid">
     <!-- start page title -->
     <div class="row">
+        <div class="col-12 mt-1" id="alerBox"></div>
         <div class="col-12">
             <div class="page-title-box">
                 <div class="page-title-right"></div>
@@ -14,13 +15,27 @@
     <!-- end page title --> 
     <div class="row mb-1">
         <div class="col-md-3 sm-m-3">
+            <label for="type">Report Type</label>
+            <select name="type" id="type" class="form-select form-select-sm mandatory">
+                <option value="all">All</option>
+                <option value="bycenter">By Center</option>
+                <option value="bycourse">By Course</option>
+            </select>
+            <span class="invalid-feedback type"></span>
+        </div>
+        <div class="col-md-3 sm-m-3">
+            <label for="criteria">Criteria</label>
+            <select name="criteria" id="criteria" class="form-select form-select-sm" disabled></select>
+            <span class="invalid-feedback type"></span>
+        </div>
+        <div class="col-md-3 sm-m-3">
             <label for="start">Start Date</label>
-            <input type="date" name="start" id="start" class="form-control form-control-sm">
+            <input type="date" name="start" id="start" class="form-control form-control-sm mandatory">
             <span class="invalid-feedback startspan"></span>
         </div>
         <div class="col-md-3">
             <label for="end">End Date</label>
-            <input type="date" name="end" id="end" class="form-control form-control-sm">
+            <input type="date" name="end" id="end" class="form-control form-control-sm mandatory">
             <span class="invalid-feedback endspan"></span>
         </div>
     </div> 
@@ -37,5 +52,5 @@
                        
 </div> <!-- container -->
 <?php require APPROOT .'/views/inc/layout/app/footer.php'; ?> 
-<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/reports/sales/salesReport.js"></script>                   
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/reports/sales/sales-report-v1.js"></script>                   
 <?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
