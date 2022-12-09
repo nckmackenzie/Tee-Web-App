05466f728be12699cf2a22c61e66cc73c73caed0
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-12-09 08:50:53 +0300

    change script to v1 for income-statement-v1.js

--- app/views/managementreports/incomestatement.php
+++ app/views/managementreports/incomestatement.php
@@ -33,5 +33,5 @@
     </div>                    
 </div> <!-- container -->
 <?php require APPROOT .'/views/inc/layout/app/footer.php'; ?> 
-<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/reports/management/income-statement.js"></script>                   
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/reports/management/income-statement-v1.js"></script>                   
 <?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
