2a32b013fcec84b696bb11f3d3cffdc27d20038c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-12-12 11:22:10 +0300

    add alerBox,change script file src and add select for report type

--- app/views/feereports/feepayments.php
+++ app/views/feereports/feepayments.php
@@ -4,23 +4,27 @@
 <div class="container-fluid">
     <!-- start page title -->
     <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                <div class="page-title-right"></div>
-                <h4 class="page-title"></h4>
-            </div>
+        <div class="col-12 mt-2" id="alerBox"></div>
         </div>
     </div>     
     <!-- end page title --> 
     <div class="row mb-1">
         <div class="col-md-3 sm-m-3">
+            <label for="type">Report Type</label>
+            <select name="type" id="type" class="form-select form-select-sm mandatory">
+                <option value="all">All</option>
+                <option value="bycourse">By Courses</option>
+            </select>
+            <span class="invalid-feedback startspan"></span>
+        </div>
+        <div class="col-md-3 sm-m-3">
             <label for="start">Start Date</label>
-            <input type="date" name="start" id="start" class="form-control form-control-sm">
+            <input type="date" name="start" id="start" class="form-control form-control-sm mandatory">
             <span class="invalid-feedback startspan"></span>
         </div>
         <div class="col-md-3">
             <label for="end">End Date</label>
-            <input type="date" name="end" id="end" class="form-control form-control-sm">
+            <input type="date" name="end" id="end" class="form-control form-control-sm mandatory">
             <span class="invalid-feedback endspan"></span>
         </div>
     </div> 
@@ -37,5 +41,5 @@
                        
 </div> <!-- container -->
 <?php require APPROOT .'/views/inc/layout/app/footer.php'; ?> 
-<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/reports/fees/feePayments.js"></script>                   
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/reports/fees/fee-payments-v1.js"></script>                   
 <?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
