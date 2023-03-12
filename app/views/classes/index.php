62560cee310e91b33e7c3e811eee2124def4b899
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-03-12 14:48:44 +0300

    index page for classes table

---
+++ app/views/classes/index.php
@@ -0,0 +1,64 @@
+<?php require APPROOT .'/views/inc/layout/app/header.php'; ?>
+<?php require APPROOT .'/views/inc/layout/app/sidebar.php'; ?>
+<!-- Start Content-->
+<div class="container-fluid">
+    <!-- start page title -->
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right"></div>
+                <h4 class="page-title">Classes</h4>
+            </div>
+        </div>
+    </div>     
+    <!-- end page title --> 
+<?php flash('class_msg','alert'); ?>
+<?php DeleteModal(URLROOT .'/classes/delete','centermodal','Are you sure you want to delete this class','id') ;?>
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row mb-2">
+                    <div class="col-sm-4">
+                        <a href="<?php echo URLROOT;?>/classes/add" class="btn btn-success mb-2"><i class="mdi mdi-plus-circle me-2"></i> New Class</a>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-centered dt-responsive w-100 nowrap" id="classes-datatable">
+                        <thead class="table-light">
+                            <tr>
+                                <th class="d-none">ID</th>
+                                <th>Class Name</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($data['classes'] as $class) : ?>
+                                <tr>
+                                    <td class="d-none"><?php echo $class->ID;?></td>
+                                    <td><?php echo $class->ClassName;?></td>
+                                    <td>
+                                        <?php if((int)$_SESSION['usertypeid'] < 3 ): ?>
+                                            <a href="<?php echo URLROOT;?>/classes/edit/<?php echo $class->ID;?>" class="action-icon btn text-success"> <i class="mdi mdi-square-edit-outline"></i></a>
+                                            <button class="action-icon btn text-danger btndel"
+                                                    data-id="<?php echo $class->ID;?>" 
+                                                    data-bs-toggle="modal" 
+                                                    data-bs-target="#centermodal"
+                                                        ><i class="mdi mdi-delete"></i></button>
+                                        <?php endif; ?>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div><!-- /.card-body -->
+        </div><!-- /.card -->
+    </div><!-- /.col-12 -->
+</div><!-- /.row -->
+</div> <!-- container -->
+<?php require APPROOT .'/views/inc/layout/app/footer.php'; ?> 
+<?php flash('class_flash_msg','toast');?>
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/classes/index.js"></script>
+<?php require APPROOT .'/views/inc/layout/app/end.php'; ?>                    
